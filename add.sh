1b5f42bfe8a294f6d9103c3c833573132cd0c136
William Wagner Moraes Artero <william.artero@dafiti.com.br>
2019-08-19 14:29:14 -0300

    feat: add script to add new files to the project

---
+++ add.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <tool> <file>"
+  exit 1
+fi
+
+TOOL=$1
+FILE=$2
+
+if [ -L $FILE ]; then
+  echo "$FILE is a symbolic link already"
+  exit 1
+fi
+
+if [ ! $(echo "$FILE" | grep "$HOME") ]; then
+  echo "Can't add file: it ins't in your home"
+  exit 1
+fi
+
+DESTINATION=$(echo $FILE | sed "s#$HOME#$(pwd)/$TOOL#")
+
+echo "adding $FILE to $DESTINATION"
+mkdir -p $(dirname $DESTINATION)
+cp $FILE $DESTINATION
+
+echo "Removing origin file $FILE"
+rm $FILE
+
+echo "stowing tool $TOOL"
+stow -t ~ -R $TOOL
