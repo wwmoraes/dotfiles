5ac6f4bf49256d755d033192de78f89ce1ce2914
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-08-20 23:06:15 -0300

    chore: added comments to better identify fish configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,6 @@
+# home binaries
 set PATH $HOME/.local/bin $PATH 
+
+# Powerline
 set fish_function_path $fish_function_path "$HOME/.local/lib/python2.7/site-packages/powerline/bindings/fish"
 powerline-setup
