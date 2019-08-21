05fa5c54fa281a599decfe25feb939dae70dca30
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-08-20 23:34:01 -0300

    feat: added fisher bootstrap

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,2 @@
 # home binaries
-set PATH $HOME/.local/bin $PATH 
-
-# Fisher bootstrap
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
-# Powerline
-set fish_function_path $fish_function_path "$HOME/.local/lib/python2.7/site-packages/powerline/bindings/fish"
-powerline-setup
+set PATH $HOME/.local/bin $PATH
