bfffa050a89f1a5341a0520610e6be03e08b399c
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-08-22 00:57:51 -0300

    feat: reworked dotfiles command to include subcommands

--- fish/.config/fish/functions/dotfiles.fish
+++ fish/.config/fish/functions/dotfiles.fish
@@ -1,5 +1,23 @@
-function dotfiles -d "Setup dotfiles"
-    pushd ~/.dotfiles > /dev/null
-    make install
-    popd > /dev/null
+function dotfiles -a cmd -d "Setup dotfiles"
+  switch "$cmd"
+    case install
+      _dotfiles_install
+    case status
+      _dotfiles_status
+    case "" "*"
+      echo "Unknown option $cmd"
+  end
 end
+complete -ec dotfiles
+
+function _dotfiles_install
+  pushd ~/.dotfiles > /dev/null
+  make install
+  popd > /dev/null
+end
+complete -xc dotfiles -n __fish_use_subcommand -a install -d "[re]install dotfiles"
+
+function _dotfiles_status
+  git -C ~/.dotfiles status -s
+end
+complete -xc dotfiles -n __fish_use_subcommand -a status -d "check dotfiles repository status"
