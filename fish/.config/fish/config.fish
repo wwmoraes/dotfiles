c901946ae3bdb996607e4a9772700f543a5ad235
William Wagner Moraes Artero <william.artero@dafiti.com.br>
2019-08-27 18:58:03 -0300

    feat: fzf confs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,6 @@
 # home binaries
 set PATH $HOME/.local/bin $PATH
+
+set -x FZF_LEGACY_KEYBINDINGS 0
+set -x FZF_COMPLETE 1
+set -x FZF_REVERSE_ISEARCH_OPTS '--preview-window=up:10 --preview="echo {}" --height 100%'
