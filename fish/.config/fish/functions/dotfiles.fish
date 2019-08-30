8bc374bd41b4baccdac2f4774788a136656e8ac7
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-08-29 22:06:01 -0300

    feat: code, lg and setup dotfiles subcommands

--- fish/.config/fish/functions/dotfiles.fish
+++ fish/.config/fish/functions/dotfiles.fish
@@ -1,11 +1,16 @@
+# dotfiles main command
 function dotfiles -a cmd -d "Setup dotfiles"
   switch "$cmd"
     case install
       _dotfiles_install
-    case push
-      _dotfiles_push
-    case pull
-      _dotfiles_pull
+    case setup
+      _dotfiles_setup
+    case update
+      _dotfiles_update
+    case code
+      _dotfiles_code
+    case lg
+      _dotfiles_lg
     case status
       _dotfiles_status
     case "" "*"
@@ -14,6 +19,7 @@ function dotfiles -a cmd -d "Setup dotfiles"
 end
 complete -ec dotfiles
 
+# install subcommand
 function _dotfiles_install
   pushd ~/.dotfiles > /dev/null
   make install
@@ -21,20 +27,37 @@ function _dotfiles_install
 end
 complete -xc dotfiles -n __fish_use_subcommand -a install -d "[re]install dotfiles"
 
-function _dotfiles_push
+# setup subcommand
+function _dotfiles_setup
   pushd ~/.dotfiles > /dev/null
-  git push
+  bash ./setup.sh
   popd > /dev/null
 end
-complete -xc dotfiles -n __fish_use_subcommand -a push -d "push dotfiles"
+complete -xc dotfiles -n __fish_use_subcommand -a setup -d "setup environment for dotfiles"
 
-function _dotfiles_pull
+# update subcommand
+function _dotfiles_update
   pushd ~/.dotfiles > /dev/null
   git pull
   popd > /dev/null
 end
-complete -xc dotfiles -n __fish_use_subcommand -a pull -d "pull dotfiles"
+complete -xc dotfiles -n __fish_use_subcommand -a update -d "get latest version of dotfiles"
 
+# code subcommand
+function _dotfiles_code
+  code ~/.dotfiles
+end
+complete -xc dotfiles -n __fish_use_subcommand -a code -d "open VSCode on dotfiles' repository"
+
+# lg subcommand
+function _dotfiles_lg
+  pushd ~/.dotfiles > /dev/null
+  lazygit
+  popd > /dev/null
+end
+complete -xc dotfiles -n __fish_use_subcommand -a lg -d "open lazygit at dotfiles repository"
+
+# status subcommand
 function _dotfiles_status
   git -C ~/.dotfiles status -s
 end
