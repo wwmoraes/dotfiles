e553ef6d410e2030a66a4725ac108d38df94a93e
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-09-22 16:54:18 -0300

    feat: tmux plugin manager & plugins setup

---
+++ .setup.d/11-tmux-plugins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -e "\e[1;34mTmux extensions\e[0m"
+
+echo -e "Checking \e[96mtpm\e[0m..."
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+echo -e "Installing tpm plugins..."
+~/.tmux/plugins/tpm/bin/install_plugins
