59ea0e5a7001d0269a4d34e5a4ef1a9f5ebfdc85
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-11-05 10:47:01 +0100

    fix: MacOS

--- fish/.config/fish/conf.d/powerline.fish
+++ fish/.config/fish/conf.d/powerline.fish
@@ -1,6 +1,8 @@
 # Add powerline function bindings
 if test -d "/usr/share/powerline"
 set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
+else if test -d "/usr/local/lib/python3.7/site-packages"
+set fish_function_path $fish_function_path "/usr/local/lib/python3.7/site-packages/powerline/bindings/fish"
 else
 set fish_function_path $fish_function_path "$HOME/.local/lib/python2.7/site-packages/powerline/bindings/fish"
 end
