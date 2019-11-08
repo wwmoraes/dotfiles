5ed7deea881497af94f4af5f91a640f1f47c2621
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-11-08 11:00:00 +0100

    style: 2-space indentation

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,15 +1,15 @@
 # Abbreviations available only in interactive shells
 if status --is-interactive
-    # git abbreviations
-    if test (type -q git)
-        abbr -a -g gco git checkout
-    end
+  # git abbreviations
+  if test (type -q git)
+    abbr -a -g gco git checkout
+  end
 
-    # kubectl abbreviations
-    if test (type -q kubectl)
-        abbr -a -g k kubectl
-        abbr -a -g kgp kubectl get pods
-    end
+  # kubectl abbreviations
+  if test (type -q kubectl)
+    abbr -a -g k kubectl
+    abbr -a -g kgp kubectl get pods
+  end
 
   # work abbreviations
   if test (hostname) = "Williams-MacBook-Pro.local"
