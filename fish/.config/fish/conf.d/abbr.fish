f5d95c7085eeccab653bc57abc439ef37a4fe3fa
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-11-14 14:35:46 +0100

    feat: k8s abbreviations

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -8,7 +8,29 @@ if status --is-interactive
   # kubectl abbreviations
   if test (type -q kubectl)
     abbr -a -g k kubectl
-    abbr -a -g kgp kubectl get pods
+    abbr -a -g kgpo kubectl get pods
+    abbr -a -g kging kubectl get ingress
+    abbr -a -g kgsvc kubectl get services
+    abbr -a -g kgcm kubectl get configmaps
+    abbr -a -g kgs kubectl get secrets
+    abbr -a -g kgds kubectl get daemonsets
+    abbr -a -g kgd kubectl get deployments
+    abbr -a -g kgpv kubectl get persistentvolumes
+    abbr -a -g kgpvc kubectl get persistentvolumeclaims
+    abbr -a -g kgsa kubectl get serviceaccounts
+    abbr -a -g kgns kubectl get namespaces
+    abbr -a -g kgno kubectl get nodes
+    abbr -a -g kgcrd kubectl get customresourcedefinitions
+    abbr -a -g kgsts kubectl get statefulsets
+    abbr -a -g kghpa kubectl get horizontalpodautoscalers
+    abbr -a -g kgcj kubectl get cronjobs
+    abbr -a -g kgj kubectl get jobs
+    abbr -a -g kgnetpol kubectl get networkpolicies
+    abbr -a -g kgr kubectl get roles
+    abbr -a -g kgrb kubectl get rolebindings
+    abbr -a -g kgcr kubectl get clusterroles
+    abbr -a -g kgcrb kubectl get clusterrolebindings
+    abbr -a -g kgsc kubectl get storageclasses
   end
 
   # work abbreviations
