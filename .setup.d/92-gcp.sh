87f45e49693096406e3de9dc22e4d846f2df5324
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-12-03 21:42:21 +0100

    feat: gcp login, app login & config helper

--- .setup.d/92-gcp.sh
+++ .setup.d/92-gcp.sh
@@ -7,4 +7,13 @@ type -p gcloud &> /dev/null
 if [ $? -ne 0 ]; then
   printf "Downloading and installing \e[96mGoogle SDK\e[0m...\n"
   curl https://sdk.cloud.google.com | bash -- --disable-prompts --install-dir=$HOME/.local/
+
+  printf "Authenticating \e[96mgcloud\e[0m user...\n"
+  gcloud auth login
+
+  printf "Authenticating \e[96mgcloud\e[0m application-default...\n"
+  gcloud auth application-default login
+
+  printf "Setting up \e[96mgcloud\e[0m config-helper...\n"
+  gcloud config config-helper
 fi
