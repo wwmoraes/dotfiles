bad27a791f547dbcc8713030c65a67291c7c9843
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-12-08 16:04:12 +0100

    feat: fish funny greeting

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,5 @@
 # Remove the global version, as it shadows the universal one
 set -eg fish_user_paths
+
+# Funny greeting messages
+set -U fish_greeting (curl -fs --max-time 0.3 http://whatthecommit.com/index.txt; or echo "")
