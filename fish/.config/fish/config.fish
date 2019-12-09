debc7d9c4805b22c4d89376e07bfead4d14088f9
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-12-09 14:50:46 +0100

    feat: user keybindings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,8 @@ set -eg fish_user_paths
 
 # Funny greeting messages
 set -U fish_greeting (curl -fs --max-time 0.3 http://whatthecommit.com/index.txt; or echo "")
+
+function fish_user_key_bindings
+  # Ctrl-S: pet-select
+  bind \cs 'pet-select'
+end
