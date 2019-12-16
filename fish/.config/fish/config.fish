56f81df493d65f9d6b7004a4b62d627f8d7106ef
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-12-16 20:25:10 +0100

    refactor: fish user key bindings on a separate file

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,9 +4,5 @@ set -eg fish_user_paths
 # Funny greeting messages
 set -U fish_greeting (curl -fs --max-time 0.3 http://whatthecommit.com/index.txt; or echo "")
 
-function fish_user_key_bindings
-  # Ctrl-S: pet-select
-  bind \cs 'pet-select'
-end
 # try to launch tmux
 launch-tmux
---
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,4 @@
+function fish_user_key_bindings
+  # Ctrl-S: pet-select
+  bind \cs 'pet-select'
+end
