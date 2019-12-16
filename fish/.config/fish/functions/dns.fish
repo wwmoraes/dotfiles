f45d42290a06fa0ec65d028b03bbd1bec83134ca
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-12-16 15:46:30 +0100

    fix: domain fn

--- fish/.config/fish/functions/dns.fish
+++ fish/.config/fish/functions/dns.fish
@@ -3,15 +3,15 @@ function dns -d "Compact DNS results"
         echo "Missing required argument: hostname"
         return 1
     end
-    
+
     dig +nocmd (domain $argv[1]) any +multiline +noall +answer
 end
 
 function domain -d "extract domain from a given URL"
-    set -l parts (string split / -- $argv[1])
+    set -l parts (string split / -- (string replace -r "^[a-z]+://" "" $argv[1]))
     set -l domain $parts[1]
 
-    if test -z "$domain" 
+    if test -z "$domain"
         domain=$argv[1]
     end
 
