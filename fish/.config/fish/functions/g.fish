b9df1fc00d7b2babb643bf0240cdfcdad8745da7
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2019-12-20 11:32:54 +0100

    feat: g, shorter go with useful commands that Google forgot

---
+++ fish/.config/fish/functions/g.fish
@@ -0,0 +1,27 @@
+function g -a cmd -d "shorter go with useful commands that Google forgot" -w go
+  switch "$cmd"
+  case unget
+    test (count $argv) -lt 2; and echo "usage: g $argv[1] [packages]" && return 1
+
+    echo go: removing $argv[2]
+
+    # thank you go-import-redirector for the brilliant idea of having a HTTP meta header redirection
+    set -l package (curl -fsSL $argv[2] | awk 'match($0, /go-import content=.* git https:\/\/(.*)"/, m) {print m[1]}')
+    test $package = ""; and set -l package $argv[2]
+
+    # better safe than sorry
+    set -q GOPATH; or set -l GOPATH (go env GOPATH)
+
+    set -l splitPackage (string split / $package)
+    # remove src
+    test -d $GOPATH/src/$package; and rm -rf $GOPATH/src/$package
+    # remove bin
+    test -f $GOPATH/bin/$splitPackage[3]; and rm -rf $GOPATH/bin/$splitPackage[3]
+    # remove cache
+    test -d $GOPATH/pkg/mod/cache/download/$argv[2]; and rm -rf $GOPATH/pkg/mod/cache/download/$argv[2]
+  case "" "*"
+    go $argv
+  end
+end
+
+complete -xc g -n __fish_use_subcommand -a unget -d "removes dependencies and its build artifacts"
