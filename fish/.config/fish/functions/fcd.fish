979effed9ed5630159cc44abf3c7699bb7d6a6ce
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2020-01-09 14:02:29 +0100

    feat: fcd, changes to a git repository folder

--- fish/.config/fish/functions/fcd.fish
+++ fish/.config/fish/functions/fcd.fish
@@ -1,19 +1,6 @@
-function fcd -d "Fuzzy change directory"
-    if set -q argv[1]
-        set searchdir $argv[1]
-    else
-        set searchdir $HOME
-    end
+function fcd -d "Fuzzy change directory to a git-enabled project one"
+  set -l path (fls)
+  test (string length $path || echo 0) -ne 0; or return
 
-    # https://github.com/fish-shell/fish-shell/issues/1362
-    set -l tmpfile (mktemp)
-    find $searchdir \( ! -regex '.*/\..*' \) ! -name __pycache__ -type d | fzf > $tmpfile
-    set -l destdir (cat $tmpfile)
-    rm -f $tmpfile
-
-    if test -z "$destdir"
-        return 1
-    end
-
-    cd $destdir
+  cd "$path"
 end
