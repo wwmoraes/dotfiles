65b1cfb9f848c5dc63985b5f0783cf25d32da51e
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2020-01-09 14:02:15 +0100

    refactor: one-liner type checking for flg

--- fish/.config/fish/functions/flg.fish
+++ fish/.config/fish/functions/flg.fish
@@ -1,8 +1,5 @@
 function flg -d "Fuzzy launches lazygit on all git-enabled subfolders"
-  if not type -q lazygit
-    echo "please install lazygit to use this function"
-    return 1
-  end
+  type -q lazygit; or echo "please install lazygit to use this function" && return 1
 
   set -l path (fls)
   test (string length $path || echo 0) -ne 0; or return
