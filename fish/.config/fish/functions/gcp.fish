a6c71865f61472d876d3c57935f06d9226bf8ea1
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2020-01-13 10:57:11 +0100

    feat: gcp logs open stackdriver logs for the chosen pod

--- fish/.config/fish/functions/gcp.fish
+++ fish/.config/fish/functions/gcp.fish
@@ -39,6 +39,46 @@ function gcp -a cmd -d "gloud CLI wrapper with extra commands"
       or set project (gcloud config get-value core/project | tail -n 1)
 
     ext-open "https://console.cloud.google.com/home/dashboard?project=$project"
+  case logs
+    set interactive (not contains -- -i $argv; echo $status)
+
+    set project (gcloud config get-value core/project | tail -n 1)
+    # set defaults for non-interactive usage
+    if test $interactive -eq 0
+      test (string length $project || echo 0) -eq 0; and return 2
+
+      kubectl config get-contexts | awk '$1 == "*"' | awk '{print $2,$3,$5}' | read contextName contextCluster k8sNamespace
+      test (string length $contextName || echo 0) -eq 0; and return 2
+      test (string length $contextCluster || echo 0) -eq 0; and return 2
+      test (string length $k8sNamespace || echo 0) -eq 0; and return 2
+
+      echo $contextCluster | sed -E 's/gke_'$project'_([^_]+)_([^_]+)/\2 \1/' | read cluster location
+      test (string length $cluster || echo 0) -eq 0; and return 2
+      test (string length $location || echo 0) -eq 0; and return 2
+    else
+      set project (gcloud projects list | \
+        sed -E "s/(^$project .*)/"(set_color yellow)"\1"(set_color normal)"/" | \
+        fzf --ansi --header-lines=1 --prompt="GCP Project > " | \
+        awk '{print $1}')
+      test (string length $project || echo 0) -eq 0; and return 2
+
+      gcloud --project=$project container clusters list 2> /dev/null | fzf -1 --header-lines=1 --prompt="k8s cluster > " | awk '{print $1,$2}' | read cluster location
+      test (string length $cluster || echo 0) -eq 0; and return 2
+      test (string length $location || echo 0) -eq 0; and return 2
+
+      set contextName (kubectl config get-contexts | awk '$1 == "*" || NR == 1 {print $2,$3;next};{print $1,$2}' | column -t | grep -e "NAME.*CLUSTER" -e "gke_"$project"_"$location"_"$cluster | fzf -1 --header-lines=1 --prompt="k8s context > " | awk '{print $1}')
+      test (string length $contextName || echo 0) -eq 0; and return 2
+
+      set k8sNamespace (kubectl --context=$contextName get namespaces | grep -v -helm | fzf --header-lines=1 --prompt="k8s namespace > " | awk '{print $1}')
+      test (string length $k8sNamespace || echo 0) -eq 0; and return 2
+    end
+
+    set prompt "Listing pods at GCP project "(set_color yellow)$project(set_color normal)" -> k8s cluster "(set_color yellow)$cluster(set_color normal)" -> namespace "(set_color yellow)$k8sNamespace(set_color normal)
+    printf $prompt
+    set k8sPodName (kubectl --context=$contextName get pods -n $k8sNamespace | fzf --header-lines=1 --prompt="k8s pod > " | awk '{print $1}')
+    test (string length $k8sPodName || echo 0) -eq 0; and return 2
+
+    ext-open "https://console.cloud.google.com/logs/viewer?interval=P7D&project=$project&minLogLevel=0&expandAll=false&customFacets=&limitCustomFacetWidth=true&advancedFilter=resource.type%3D%22k8s_container%22%0Aresource.labels.project_id%3D%22$project%22%0Aresource.labels.location%3D%22$location%22%0Aresource.labels.cluster_name%3D%22$cluster%22%0Aresource.labels.namespace_name%3D%22$k8sNamespace%22%0Aresource.labels.pod_name%3D%22$k8sPodName%22"
   case "*"
     gcloud $argv
   end
@@ -51,3 +91,4 @@ complete -xc gcp -n __fish_use_subcommand -a setup -d "setup docker auth and the
 complete -xc gcp -n __fish_use_subcommand -a project -d "fuzzy set project"
 complete -xc gcp -n __fish_use_subcommand -a "region zone" -d "fuzzy set region and zone"
 complete -xc gcp -n __fish_use_subcommand -a dashboard -d "open the current/passed project dashboard"
+complete -xc gcp -n __fish_use_subcommand -a logs -d "open the stackdriver logs viewer for the chosen pod"
