51249f6ce063db9295f65d1a84cf1e996d54a524
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2020-01-30 22:20:18 +0100

    refactor: gcp

--- fish/.config/fish/functions/gcp.fish
+++ fish/.config/fish/functions/gcp.fish
@@ -8,77 +8,17 @@ function gcp -a cmd -d "gloud CLI wrapper with extra commands"
 
   switch "$cmd"
   case reauth
-    echo "authenticating user..."
-    gcloud -q --no-user-output-enabled auth login --brief
-    echo "authenticating application-default..."
-    gcloud -q --no-user-output-enabled auth application-default login 2> /dev/null
+    _gcp_reauth
   case setup
-    echo "configuring docker..."
-    gcloud -q --no-user-output-enabled auth configure-docker 2> /dev/null
-    echo "setting up config-helper..."
-    gcloud -q --no-user-output-enabled config config-helper 2> /dev/null
+    _gcp_setup
   case project
-    set active (gcloud config get-value core/project | tail -n 1)
-    set selected (gcloud projects list | \
-      sed -E "s/(^$active .*)/"(set_color yellow)"\1"(set_color normal)"/" | \
-      fzf --ansi --header-lines=1 | \
-      awk '{print $1}')
-    test -z "$selected"; or gcloud config set project $selected
+    _gcp_project
   case region zone
-    set selected (gcloud compute zones list | fzf --ansi --header-lines=1)
-    test -z "$selected"; and return
-
-    set zone (echo $selected | awk '{print $1}')
-    set region (echo $selected | awk '{print $2}')
-
-    gcloud config set compute/zone $zone
-    gcloud config set compute/region $region
+    _gcp_regionzone
   case dashboard
-    test (count $argv) -eq 2;
-      and set project $argv[2];
-      or set project (gcloud config get-value core/project | tail -n 1)
-
-    ext-open "https://console.cloud.google.com/home/dashboard?project=$project"
+    _gcp_dashboard $argv[2..-1]
   case logs
-    set interactive (not contains -- -i $argv; echo $status)
-
-    set project (gcloud config get-value core/project | tail -n 1)
-    # set defaults for non-interactive usage
-    if test $interactive -eq 0
-      test (string length $project || echo 0) -eq 0; and return 2
-
-      kubectl config get-contexts | awk '$1 == "*"' | awk '{print $2,$3,$5}' | read contextName contextCluster k8sNamespace
-      test (string length $contextName || echo 0) -eq 0; and return 2
-      test (string length $contextCluster || echo 0) -eq 0; and return 2
-      test (string length $k8sNamespace || echo 0) -eq 0; and return 2
-
-      echo $contextCluster | sed -E 's/gke_'$project'_([^_]+)_([^_]+)/\2 \1/' | read cluster location
-      test (string length $cluster || echo 0) -eq 0; and return 2
-      test (string length $location || echo 0) -eq 0; and return 2
-    else
-      set project (gcloud projects list | \
-        sed -E "s/(^$project .*)/"(set_color yellow)"\1"(set_color normal)"/" | \
-        fzf --ansi --header-lines=1 --prompt="GCP Project > " | \
-        awk '{print $1}')
-      test (string length $project || echo 0) -eq 0; and return 2
-
-      gcloud --project=$project container clusters list 2> /dev/null | fzf -1 --header-lines=1 --prompt="k8s cluster > " | awk '{print $1,$2}' | read cluster location
-      test (string length $cluster || echo 0) -eq 0; and return 2
-      test (string length $location || echo 0) -eq 0; and return 2
-
-      set contextName (kubectl config get-contexts | awk '$1 == "*" || NR == 1 {print $2,$3;next};{print $1,$2}' | column -t | grep -e "NAME.*CLUSTER" -e "gke_"$project"_"$location"_"$cluster | fzf -1 --header-lines=1 --prompt="k8s context > " | awk '{print $1}')
-      test (string length $contextName || echo 0) -eq 0; and return 2
-
-      set k8sNamespace (kubectl --context=$contextName get namespaces | grep -v -helm | fzf --header-lines=1 --prompt="k8s namespace > " | awk '{print $1}')
-      test (string length $k8sNamespace || echo 0) -eq 0; and return 2
-    end
-
-    set prompt "Listing pods at GCP project "(set_color yellow)$project(set_color normal)" -> k8s cluster "(set_color yellow)$cluster(set_color normal)" -> namespace "(set_color yellow)$k8sNamespace(set_color normal)
-    printf $prompt
-    set k8sPodName (kubectl --context=$contextName get pods -n $k8sNamespace | fzf --header-lines=1 --prompt="k8s pod > " | awk '{print $1}')
-    test (string length $k8sPodName || echo 0) -eq 0; and return 2
-
-    ext-open "https://console.cloud.google.com/logs/viewer?interval=P7D&project=$project&minLogLevel=0&expandAll=false&customFacets=&limitCustomFacetWidth=true&advancedFilter=resource.type%3D%22k8s_container%22%0Aresource.labels.project_id%3D%22$project%22%0Aresource.labels.location%3D%22$location%22%0Aresource.labels.cluster_name%3D%22$cluster%22%0Aresource.labels.namespace_name%3D%22$k8sNamespace%22%0Aresource.labels.pod_name%3D%22$k8sPodName%22"
+    _gcp_logs
   case "*"
     gcloud $argv
   end
@@ -86,9 +26,126 @@ end
 
 complete -ec gcp
 complete -c gcp -w gcloud
+
+# reauth subcommand
+function _gcp_reauth
+  # if not type -q op
+  #   echo "error: op tool not found - please install 1password CLI"
+  #   echo "https://1password.com/downloads/command-line/"
+  #   return 1
+  # end
+  # if not type -q jq
+  #   echo "error: jq tool not found - please install or add to path"
+  #   echo "https://stedolan.github.io/jq/"
+  #   return 1
+  # end
+  # echo "checking 1Password..."
+  # op list items > /dev/null ^&1; or eval (op signin messagebird)
+  # set status 0
+  # echo "fetching user email..."
+  # set -l email (dscl . -read ~/ | grep @messagebird.com | sed -E 's/.*>(.*)<.*/\1/')
+  # echo "fetching password..."
+  # set -l password (op get item google | jq -er ".details.fields[] | select(.designation==\"password\") | .value")
+  set -q EMAIL; or echo "EMAIL is not set" && return 1
+
+  echo "authenticating user..."
+  gcloud -q --no-user-output-enabled auth login --brief $EMAIL 2> /dev/null
+
+  echo "authenticating application-default..."
+  gcloud -q --no-user-output-enabled auth application-default login 2> /dev/null
+end
 complete -xc gcp -n __fish_use_subcommand -a reauth -d "reauthenticate everything"
+complete -xc gcp -n '__fish_seen_subcommand_from reauth'
+
+# setup subcommand
+function _gcp_setup
+  echo "configuring docker..."
+  gcloud -q --no-user-output-enabled auth configure-docker 2> /dev/null
+  echo "setting up config-helper..."
+  gcloud -q --no-user-output-enabled config config-helper 2> /dev/null
+end
 complete -xc gcp -n __fish_use_subcommand -a setup -d "setup docker auth and the config-helper agent"
+complete -xc gcp -n '__fish_seen_subcommand_from setup'
+
+# project subcommand
+function _gcp_project
+  set active (gcloud config get-value core/project | tail -n 1)
+  set selected (gcloud projects list | \
+    sed -E "s/(^$active .*)/"(set_color yellow)"\1"(set_color normal)"/" | \
+    fzf --ansi --header-lines=1 | \
+    awk '{print $1}')
+  test -z "$selected"; or gcloud config set project $selected
+end
 complete -xc gcp -n __fish_use_subcommand -a project -d "fuzzy set project"
+complete -xc gcp -n '__fish_seen_subcommand_from project'
+
+# region/zone subcommand
+function _gcp_regionzone
+  set selected (gcloud compute zones list | fzf --ansi --header-lines=1)
+  test -z "$selected"; and return
+
+  set zone (echo $selected | awk '{print $1}')
+  set region (echo $selected | awk '{print $2}')
+
+  gcloud config set compute/zone $zone
+  gcloud config set compute/region $region
+end
 complete -xc gcp -n __fish_use_subcommand -a "region zone" -d "fuzzy set region and zone"
-complete -xc gcp -n __fish_use_subcommand -a dashboard -d "open the current/passed project dashboard"
+complete -xc gcp -n '__fish_seen_subcommand_from region zone'
+
+# dashboard subcommand
+function _gcp_dashboard
+  test (count $argv) -gt 0;
+    and set project $argv[1];
+    or set project (gcloud config get-value core/project | tail -n 1)
+
+  ext-open "https://console.cloud.google.com/home/dashboard?project=$project"
+end
+complete -xc gcp -n __fish_use_subcommand -a dashboard -d "open dashboard for the given project name (or current one if none given)"
+complete -xc gcp -n '__fish_seen_subcommand_from dashboard'
+
+# logs subcommand
+function _gcp_logs
+  set interactive (not contains -- -i $argv; echo $status)
+
+  set project (gcloud config get-value core/project | tail -n 1)
+  # set defaults for non-interactive usage
+  if test $interactive -eq 0
+    test (string length $project || echo 0) -eq 0; and return 2
+
+    kubectl config get-contexts | awk '$1 == "*"' | awk '{print $2,$3,$5}' | read contextName contextCluster k8sNamespace
+    test (string length $contextName || echo 0) -eq 0; and return 2
+    test (string length $contextCluster || echo 0) -eq 0; and return 2
+    test (string length $k8sNamespace || echo 0) -eq 0; and return 2
+
+    echo $contextCluster | sed -E 's/gke_'$project'_([^_]+)_([^_]+)/\2 \1/' | read cluster location
+    test (string length $cluster || echo 0) -eq 0; and return 2
+    test (string length $location || echo 0) -eq 0; and return 2
+  else
+    set project (gcloud projects list | \
+      sed -E "s/(^$project .*)/"(set_color yellow)"\1"(set_color normal)"/" | \
+      fzf --ansi --header-lines=1 --prompt="GCP Project > " | \
+      awk '{print $1}')
+    test (string length $project || echo 0) -eq 0; and return 2
+
+    gcloud --project=$project container clusters list 2> /dev/null | fzf -1 --header-lines=1 --prompt="k8s cluster > " | awk '{print $1,$2}' | read cluster location
+    test (string length $cluster || echo 0) -eq 0; and return 2
+    test (string length $location || echo 0) -eq 0; and return 2
+
+    set contextName (kubectl config get-contexts | awk '$1 == "*" || NR == 1 {print $2,$3;next};{print $1,$2}' | column -t | grep -e "NAME.*CLUSTER" -e "gke_"$project"_"$location"_"$cluster | fzf -1 --header-lines=1 --prompt="k8s context > " | awk '{print $1}')
+    test (string length $contextName || echo 0) -eq 0; and return 2
+
+    set k8sNamespace (kubectl --context=$contextName get namespaces | grep -v -helm | fzf --header-lines=1 --prompt="k8s namespace > " | awk '{print $1}')
+    test (string length $k8sNamespace || echo 0) -eq 0; and return 2
+  end
+
+  set prompt "Listing pods at GCP project "(set_color yellow)$project(set_color normal)" -> k8s cluster "(set_color yellow)$cluster(set_color normal)" -> namespace "(set_color yellow)$k8sNamespace(set_color normal)
+  printf $prompt
+  set k8sPodName (kubectl --context=$contextName get pods -n $k8sNamespace | fzf --header-lines=1 --prompt="k8s pod > " | awk '{print $1}')
+  test (string length $k8sPodName || echo 0) -eq 0; and return 2
+
+  ext-open "https://console.cloud.google.com/logs/viewer?interval=P7D&project=$project&minLogLevel=0&expandAll=false&customFacets=&limitCustomFacetWidth=true&advancedFilter=resource.type%3D%22k8s_container%22%0Aresource.labels.project_id%3D%22$project%22%0Aresource.labels.location%3D%22$location%22%0Aresource.labels.cluster_name%3D%22$cluster%22%0Aresource.labels.namespace_name%3D%22$k8sNamespace%22%0Aresource.labels.pod_name%3D%22$k8sPodName%22"
+end
 complete -xc gcp -n __fish_use_subcommand -a logs -d "open the stackdriver logs viewer for the chosen pod"
+complete -xc gcp -n '__fish_seen_subcommand_from logs' -s i -d "interactively selects all parameters instead of autodiscovering"
+complete -xc gcp -n '__fish_seen_subcommand_from logs'
