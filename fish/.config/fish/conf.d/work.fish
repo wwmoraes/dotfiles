ab8b71d867cd3e86f1b2270812534ae47ce3c81e
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2020-04-19 11:11:39 +0200

    fix: work-setup renames k8s contexts and set default ns correctly

--- fish/.config/fish/conf.d/work.fish
+++ fish/.config/fish/conf.d/work.fish
@@ -9,15 +9,15 @@ if type -q kubectl
     printf "Fixing kubectl contexts...\n"
     kubectl config get-contexts | \
       tail -n +2 | \
-      awk '$1 ~ /\*/ {print $2};$1 !~ /\*/ {print $1}' | \
-      sed -E 's/(gke_mb-k8s-([a-z]+)_.*_([a-z0-9]+))/\1 \2-\3/; s/(gke_messagebird-([a-z]+)_[^_]*_([a-z0-9-]+))/\1 \2-\3/' | \
+      awk '$1 ~ /\*/ {print $2};$1 !~ /\*/ {print $2,$1}' | \
+      sed -E 's/gke_mb-([a-z0-9-]+)_.*_([a-z0-9]+) (.*)/\3 \1-\2/; s/gke_messagebird-live_[^_]*_([a-z0-9-]+) (.*)/\2 old-\1/' | \
       awk 'NF == 2' | \
       xargs -I % sh -c 'kubectl config rename-context %'
-    printf "setting default namespace for contexts without a namespace...\n"
-    kubectl config get-contexts | \
-      tail +2 | \
-      awk '$1 == "*" {$1=""};1' | \
-      awk 'NF == 3 {print $1}' | \
-      xargs -I % kubectl config set-context % --namespace=default
+  printf "setting default namespace for contexts without a namespace...\n"
+  kubectl config get-contexts | \
+    tail +2 | \
+    awk '$1 == "*" {$1=""};1' | \
+    awk 'NF == 3 {print $1}' | \
+    xargs -I % kubectl config set-context % --namespace=default
   end
 end
