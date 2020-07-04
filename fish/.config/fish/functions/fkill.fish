dbcb57c7c4d8c6f410f67ecb27ad0d8fb9682b78
William Wagner Moraes Artero <williamwmoraes@gmail.com>
2020-07-04 15:38:08 +0200

    lint: fkill indentation

--- fish/.config/fish/functions/fkill.fish
+++ fish/.config/fish/functions/fkill.fish
@@ -1,7 +1,7 @@
 function fkill -d "Fuzzy kill"
-    set pid (ps $argv -o pid,user,command | sed 1d | fzf -m | awk '{print $1}')
+  set pid (ps $argv -o pid,user,command | sed 1d | fzf -m | awk '{print $1}')
 
-    if test -n "$pid"
-        echo $pid | xargs kill
-    end
+  if test -n "$pid"
+    echo $pid | xargs kill
+  end
 end
