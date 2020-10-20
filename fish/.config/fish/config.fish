f5f0c64ebae1d7a6a4b7680c5ba3ead8530534c2
William Artero <williamwmoraes@gmail.com>
2020-10-20 23:33:33 +0200

    feat: wakatime

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,5 @@ set -eg fish_user_paths
 
 # try to launch tmux
 launch-tmux
+
+source ~/.config/fish/functions/fish_prompt.fish
---
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,16 @@
+function __fish_preexec_wakatime --on-event fish_preexec
+  set -l project "terminal"
+
+  test -d $PWD/.git; and begin
+    if not test -f $PWD/.wakatime-project
+      set project (git -C $PWD remote get-url (git -C $PWD remote | head -1))
+      set project (string replace -r "git@(.*):(.*)\.git" "\$1/\$2" $project)
+      echo $project > $PWD/.wakatime-project
+    else
+      set project (cat $PWD/.wakatime-project)
+    end
+  end
+
+  wakatime --write --plugin "fish-wakatime/0.0.1" --entity-type app --project "$project" --entity (echo $argv | cut -d ' ' -f1) 2>&1 > /dev/null &
+  disown (jobs -lp | tail +1)
+end
