e0bacdfdbe1b0370dbb551924f382b8b4007fe1f
William Artero <williamwmoraes@gmail.com>
2020-11-02 18:45:17 +0100

    refactor: watchrun now supports multiple watched files and folders

--- fish/.config/fish/functions/watchrun.fish
+++ fish/.config/fish/functions/watchrun.fish
@@ -1,8 +1,17 @@
-function watchrun -a path -d "watch for file changes and run command on events"
+function watchrun -d "watch for file changes and run command on events"
   if test (count $argv) -lt 2
     echo "usage: "(status function)" <path> <command>"
+    echo "usage: "(status function)" <path1>..[pathN] -- <command>"
     return 2
   end
-  set -e argv[1]
-  fswatch -o $path | xargs -n1 -I{} $argv
+
+  if set -l index (contains -i -- "--" $argv)
+    set paths $argv[1..(expr $index - 1)]
+    set cmd $argv[(expr $index + 1)..-1]
+  else
+    set paths $argv[1]
+    set cmd $argv[2..-1]
+  end
+
+  fswatch -o $paths | xargs -n 1 sh -c "clear; date; $cmd"
 end
