1d4a77792fffa5fd02cee0b276b2f0081e849c38
William Artero <williamwmoraes@gmail.com>
2020-11-30 18:22:20 +0100

    fix: wakatime preexec on prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,16 +1,25 @@
 function __fish_preexec_wakatime --on-event fish_preexec
   set -l project "terminal"
 
-  test -d $PWD/.git; and begin
+  test -d $PWD/.git
+  and begin
     if not test -f $PWD/.wakatime-project
-      set project (git -C $PWD remote get-url (git -C $PWD remote | head -1))
+      set project (git -C $PWD remote get-url (git -C $PWD remote | head -1) 2> /dev/null)
       set project (string replace -r "git@(.*):(.*)\.git" "\$1/\$2" $project)
-      echo $project > $PWD/.wakatime-project
+      echo $project >$PWD/.wakatime-project
     else
       set project (cat $PWD/.wakatime-project)
     end
   end
 
-  wakatime --write --plugin "fish-wakatime/0.0.1" --entity-type app --project "$project" --entity (echo $argv | cut -d ' ' -f1) 2>&1 > /dev/null &
-  disown (jobs -lp | tail +1)
+  test -n "$argv"
+  and begin
+    wakatime \
+      --write \
+      --plugin "fish-wakatime/0.0.1" \
+      --entity-type app \
+      --project "$project" \
+      --entity "(echo $argv | cut -d ' ' -f1)" 2>&1 >/dev/null &
+    disown (jobs -lp | tail +1)
+  end
 end
