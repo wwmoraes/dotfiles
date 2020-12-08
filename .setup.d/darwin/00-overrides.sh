b64df41c1f76dd38c07e8ca4ef6a35e7501ab9dd
William Artero <williamwmoraes@gmail.com>
2020-12-08 12:46:55 +0100

    chore: link vscode config folder on MacOS library

--- .setup.d/darwin/00-overrides.sh
+++ .setup.d/darwin/00-overrides.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 ln -sf /usr/local/bin/python3 /usr/local/bin/python
+
+# link VSCode user folder
+SOURCE="$HOME/.config/Code/User"
+TARGET="$HOME/Library/Application Support/Code/User"
+if [[ ! -L "${TARGET}" ]]; then
+  rm -rf "${TARGET}"
+  ln -sf "${SOURCE}" "${TARGET}"
+fi
