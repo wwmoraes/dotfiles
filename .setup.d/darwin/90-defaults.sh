c53a48840fe35f648f83a2f645b998a199d24411
William Artero <williamwmoraes@gmail.com>
2020-12-14 20:05:40 +0100

    chore: MacOS safari defaults update

--- .setup.d/darwin/90-defaults.sh
+++ .setup.d/darwin/90-defaults.sh
@@ -383,13 +383,13 @@ defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
 
 # Allow hitting the Backspace key to go to the previous page in history
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool false
 
-# Hide Safari’s bookmarks bar by default
+# Show Safari’s bookmarks bar by default
 defaults write com.apple.Safari ShowFavoritesBar -bool true
 
-# Hide Safari’s sidebar in Top Sites
-defaults write com.apple.Safari ShowSidebarInTopSites -bool false
+# Show Safari’s sidebar in Top Sites
+defaults write com.apple.Safari ShowSidebarInTopSites -bool true
 
 # Disable Safari’s thumbnail cache for History and Top Sites
 defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
