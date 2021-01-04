5f6312cfdf38c1e93e3e03aeb46dcc9e977d0d9d
William Artero <williamwmoraes@gmail.com>
2021-01-04 19:36:54 +0100

    chore(finicky): rewrite URLs to remove tracking query params

--- .systems/darwin/finicky/.finicky.js
+++ .systems/darwin/finicky/.finicky.js
@@ -1,5 +1,29 @@
 module.exports = {
   defaultBrowser: "Browserosaurus",
+  rewrite: [
+    {
+      match: () => true,
+      url({ url }) {
+        const removeKeysStartingWith = ["utm_", "uta_"];
+        const removeKeys = ["fblid", "gclid", "auto_subscribed", "email_source"];
+
+        const search = url.search
+          .split("&")
+          .map((parameter) => parameter.split("="))
+          .filter(([key]) =>
+            !removeKeysStartingWith.some(
+              (startingWith) => key.startsWith(startingWith)
+            )
+          )
+          .filter(([key]) => !removeKeys.some((removeKey) => key === removeKey));
+
+        return {
+          ...url,
+          search: search.map((parameter) => parameter.join("=")).join("&"),
+        };
+      },
+    }
+  ],
   handlers: [
     {
       match: ({ sourceBundleIdentifier }) =>
