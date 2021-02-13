08787fb4891b36de84a74b3ce08abc524850d8c3
William Artero <william@artero.dev>
2021-02-13 15:08:56 +0100

    feat(grc): color all-the-things!

---
+++ fish/.config/fish/conf.d/grc.fish
@@ -0,0 +1,23 @@
+# only apply grc override on interactive shells
+status --is-interactive; or exit
+
+# don't setup anything if grc is not installed
+type -q grc; or exit
+
+# create the grc wrapper functions for the enabled binaries
+for executable in (cat ~/.grc/bins.txt | grep -v ":false")
+  type -q $executable; or continue
+
+  function $executable --inherit-variable executable --wraps=$executable
+    if isatty 1
+      grc $executable $argv
+    else
+      eval command $executable $argv
+    end
+  end
+end
+
+# remove the wrapper function for explicitly disabled binaries
+for executable in (cat ~/.grc/bins.txt | grep ":false")
+  functions -e $executable
+end
