3cc6c0d695c8f9f4d7b640d864ddfb684b242a23
William Artero <william@artero.dev>
2021-02-17 08:52:26 +0100

    chore(fish): missing quiet flag on wakatime heartbeat functions check

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -15,7 +15,7 @@ function __fish_preexec_wakatime --on-event fish_preexec
     set -l commandName (echo "$argv" | cut -d ' ' -f1)
 
     ### command name transformations
-    # replace folder navigation to cd command
+    # replace folder navigation with cd command
     test -d $commandName; and set -l commandName "cd"
     # ignore if not a valid command
     type -p "$commandName" > /dev/null ^&1; or return
@@ -30,7 +30,7 @@ function __fish_preexec_wakatime --on-event fish_preexec
     # fish abbreviations
     abbr -l | grep -qx $commandName; and set languageName "fish"
     # fish functions
-    functions -n | tr ',' '\n' | grep -x $commandName; and set languageName "fish"
+    functions -n | tr ',' '\n' | grep -qx $commandName; and set languageName "fish"
     # shell scripts
     string match -r "\.fish\$" $commandName; and set languageName "fish"
     string match -r "\.sh\$" $commandName; and set languageName "shell"
