e4d72f17970c86dacbd2788e0d4c52932f8adccc
William Artero <william@artero.dev>
2021-02-19 17:43:02 +0100

    chore(setup): hardlink fonts and invalidate cache

--- .setup.d/darwin/00-overrides.sh
+++ .setup.d/darwin/00-overrides.sh
@@ -12,3 +12,11 @@ if [[ ! -L "${TARGET}" ]]; then
   rm -rf "${TARGET}"
   ln -sf "${SOURCE}" "${TARGET}"
 fi
+
+# harklink fonts
+mkdir -p "${HOME}/Library/Fonts"
+for font in "${HOME}"/.local/share/fonts/*; do
+  ln -f "${font}" "${HOME}/Library/Fonts/$(basename "${font}")"
+done
+atsutil databases -removeUser
+killall fontd
