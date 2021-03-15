c36fdc8aae9657ebbf698189e77106a68232ff67
William Artero <william@artero.dev>
2021-03-15 14:44:27 +0100

    chore(fish): update reload-config to source completions and functions as well

--- fish/.config/fish/functions/reload-config.fish
+++ fish/.config/fish/functions/reload-config.fish
@@ -1,6 +1,15 @@
-function reload-config
+function reload-config -d "force-reloads all fish config"
   source ~/.config/fish/config.fish
-  for conf in ~/.config/fish/conf.d/*.fish
-    source $conf
+
+  for confFilePath in ~/.config/fish/conf.d/*.fish
+    source $confFilePath
+  end
+
+  for completionsFilePath in ~/.config/fish/completions/*.fish
+    source $completionsFilePath
+  end
+
+  for functionFilePath in ~/.config/fish/functions/*.fish
+    source $functionFilePath
   end
 end
