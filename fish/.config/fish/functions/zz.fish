f7e575e7c200dd5f32a1193c26b260ae39cf7e9a
William Artero <william@artero.dev>
2021-03-26 20:37:19 +0100

    feat: zz, an azure CLI wrapper

---
+++ fish/.config/fish/functions/zz.fish
@@ -0,0 +1,19 @@
+function zz -a cmd -d "Azure CLI simplified - now from Z to Z"
+  type -q az; or echo "azure CLI is not installed" && return
+
+  switch "$cmd"
+    case faccount
+      az account list -o table 2>/dev/null | \
+      awk 'NR != 2' | \
+      fzf --header-lines=1 | \
+      awk '{print $(NF-2)}' | \
+      ifne xargs -I% az account set --verbose -s "%"
+      az account show
+    case "*"
+      az $argv
+  end
+end
+complete -ec zz
+complete -c zz -w az
+
+complete -xc zz -n __fish_use_subcommand -a faccount -d "fuzzy account switcher"
