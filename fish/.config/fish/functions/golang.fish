7d65588b7ca31efb0834c3fd60c72b145dcdc635
William Artero <william@artero.dev>
2021-07-31 09:38:26 +0200

    refactor(fish-golang): simplified mod removal test and action

--- fish/.config/fish/functions/golang.fish
+++ fish/.config/fish/functions/golang.fish
@@ -14,13 +14,17 @@ function golang -a cmd -d "go wrapper with commands that Google forgot" -w go
 
     set -l splitPackage (string split / $package)
     # remove src
-    test -d $GOPATH/src/$package; and rm -rf $GOPATH/src/$package
+    test -d $GOPATH/src/$package
+    and rm -rf $GOPATH/src/$package
     # remove bin
-    test -f $GOPATH/bin/$splitPackage[3]; and rm -rf $GOPATH/bin/$splitPackage[3]
+    test -f $GOPATH/bin/$splitPackage[3]
+    and rm -rf $GOPATH/bin/$splitPackage[3]
     # remove mod
-    find $GOPATH/pkg/mod/$package* -type d -maxdepth 0 -exec sudo rm -rf {} +
+    test -d $GOPATH/pkg/mod/$package
+    and rm -rf $GOPATH/pkg/mod/$package
     # remove cache
-    test -d $GOPATH/pkg/mod/cache/download/$argv[2]; and rm -rf $GOPATH/pkg/mod/cache/download/$argv[2]
+    test -d $GOPATH/pkg/mod/cache/download/$argv[2]
+    and rm -rf $GOPATH/pkg/mod/cache/download/$argv[2]
 
     # remove go.mod dependency
     if test -f go.mod
