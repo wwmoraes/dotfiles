f4b8f76cfd5178d2f4cd95cfad9dfe0e2223f9cc
William Artero <william@artero.dev>
2021-08-13 10:27:27 +0200

    fix(setup): remove override link for vscode

--- .setup.d/darwin/00-overrides.sh
+++ .setup.d/darwin/00-overrides.sh
@@ -9,14 +9,6 @@ trap 'kill 0' INT HUP TERM
 test "${TRACE:-0}" = "1" && set -x
 test "${VERBOSE:-0}" = "1" && set -v
 
-# link VSCode user folder
-SOURCE="${HOME}/.config/Code/User"
-TARGET="${HOME}/Library/Application Support/Code/User"
-if [ ! -L "${TARGET}" ]; then
-  rm -rf "${TARGET}"
-  ln -sf "${SOURCE}" "${TARGET}"
-fi
-
 # harklink fonts
 mkdir -p "${HOME}/Library/Fonts"
 for font in "${HOME}"/.local/share/fonts/*; do
