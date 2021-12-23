f4bd58556679653c6816e56ba40406882b483384
William Artero <william@artero.dev>
2021-12-23 17:43:55 +0100

    chore: fish prompt grceful fallback if powerline-go is not found

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -47,7 +47,37 @@ function __fish_preexec_wakatime --on-event fish_preexec
 end
 
 function fish_prompt
-  set -l error $status
+  set -l last_pipestatus $pipestatus
+
+  # try to use powerline-go
   set -q GOPATH; or set -l GOPATH (go env GOPATH)
-  eval $GOPATH/bin/powerline-go -error $error -jobs (count (jobs -p))
+  test -x "$GOPATH/bin/powerline-go"; and begin
+    eval "$GOPATH/bin/powerline-go" -error $last_pipestatus -jobs (count (jobs -p))
+    return
+  end
+
+  # almost vanilla from https://github.com/fish-shell/fish-shell/blob/3.1.2/share/functions/fish_prompt.fish
+  set -l normal (set_color normal)
+
+  # Color the prompt differently when we're root
+  set -l color_cwd $fish_color_cwd
+  set -l prefix
+  set -l suffix '>'
+  if contains -- $USER root toor
+      if set -q fish_color_cwd_root
+          set color_cwd $fish_color_cwd_root
+      end
+      set suffix '#'
+  end
+
+  # If we're running via SSH, change the host color.
+  set -l color_host $fish_color_host
+  if set -q SSH_TTY
+      set color_host $fish_color_host_remote
+  end
+
+  # Write pipestatus
+  set -l prompt_status (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
+
+  echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal $prompt_status $suffix " "
 end
