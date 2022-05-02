ae70366e7d8f7cc5867a29fc3ec042a731a3f90d
William Artero <git@artero.dev>
2022-05-02 17:01:47 +0200

    chore: removed work tinyproxy profile switcher

--- .systems/darwin/hammerspoon/.hammerspoon/tags/work.lua
+++ .systems/darwin/hammerspoon/.hammerspoon/tags/work.lua
@@ -2,27 +2,6 @@ require("types.hammerspoon")
 
 local logger = hs.logger.new("work", "info")
 
---- changes the tinyproxy config file link and sends a USR1 signal to the daemon
---- to reload the config
---- @param name string|nil
---- @return nil
-local function changeProxyProfile(name)
-  name = (name == nil and "direct") or name
-  local sourceFile = string.format("%s/.config/tinyproxy/tinyproxy-%s.conf", os.getenv("HOME"), name)
-  local targetFile = os.getenv("HOME") .. "/.config/tinyproxy/tinyproxy.conf"
-  logger.i("linking proxy profile config file...")
-  local output, status, type, rc = hs.execute(string.format("ln -sf '%s' '%s'", sourceFile, targetFile))
-  if status ~= true then
-    logger.e("failed to link proxy config: %s (%s %d)", output, type, rc)
-  end
-  -- TODO use the pid file and the standard kill
-  logger.i("reloading proxy config...")
-  local output, status, type, rc = hs.execute("killall -USR1 tinyproxy")
-  if status ~= true then
-    logger.e("failed to reload proxy config: %s (%s %d)", output, type, rc)
-  end
-end
-
 -- toggle Microsoft Teams mute
 hs.hotkey.bind(nil, "F19", nil, function()
   hs.eventtap.event.newKeyEvent({ "cmd", "shift" }, "m", true):post(hs.application.get("com.microsoft.teams"))
@@ -43,7 +22,6 @@ hs.network.reachability.forHostName(os.getenv("WORK_INTRANET_HOSTNAME")):setCall
   if isReachable and workVpnIsUp ~= true then
     -- VPN tunnel is up
     logger.i("VPN is up!")
-    changeProxyProfile("aab-vpn")
     -- TODO check if the keychain entry exists
     hs.execute(string.format("kinit --keychain %s@%s", os.getenv("KERBEROS_PRINCIPAL"), os.getenv("KERBEROS_REALM")))
     if workVpnIsUp == false then
@@ -53,7 +31,6 @@ hs.network.reachability.forHostName(os.getenv("WORK_INTRANET_HOSTNAME")):setCall
   elseif not isReachable and workVpnIsUp ~= false then
     -- VPN tunnel is down
     logger.i("VPN is down!")
-    changeProxyProfile("direct")
     hs.execute("kdestroy -A")
     if workVpnIsUp == true then
       hs.urlevent.openURL("hammerspoon://contexts?name=work&action=kill")
