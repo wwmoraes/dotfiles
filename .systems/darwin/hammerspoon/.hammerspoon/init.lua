6a1b2f9d920b6704a77f0ce521fe276ccb1d3623
William Artero <git@artero.dev>
2022-05-02 08:52:00 +0200

    chore(hammerspoon): moved apps to a reusable module file

--- .systems/darwin/hammerspoon/.hammerspoon/init.lua
+++ .systems/darwin/hammerspoon/.hammerspoon/init.lua
@@ -33,37 +33,6 @@ hs.spoons.use("Env", {
 ---@type Env
 spoon.Env = spoon.Env
 
-local apps = {
-  Blisk = "org.blisk.blisk",
-  Brave = "com.brave.browser",
-  BraveBeta = "com.brave.browser.beta",
-  BraveDev = "com.brave.browser.dev",
-  ChromeCanary = "com.google.chrome.canary",
-  Edge = "com.microsoft.edgemac",
-  EdgeBeta = "com.microsoft.edgemac.beta",
-  FirefoxDeveloperEdition = "org.mozilla.firefoxdeveloperedition",
-  Opera = "com.operasoftware.opera",
-  Vivaldi = "com.vivaldi.vivaldi",
-  Wavebox = "com.bookry.wavebox",
-  Teams = "com.microsoft.teams",
-  Outlook = "com.microsoft.Outlook",
-  OneDrive = "OneDrive",
-  Amethyst = "com.amethyst.Amethyst",
-  VSCode = "com.microsoft.vscode",
-  Kitty = "net.kovidgoyal.kitty",
-  AzureDevOps = "com.fluidapp.FluidApp2.AzureDevOps",
-  LinkedIn = "com.fluidapp.FluidApp2.LinkedIn",
-  Safari = "com.apple.Safari",
-  Chrome = "com.google.Chrome",
-  Firefox = "org.mozilla.firefox",
-  VSCodium = "com.visualstudio.code.oss",
-  Slack = "com.tinyspeck.slackmacgap",
-  Messenger = "com.facebook.archon",
-  WhatsApp = "WhatsApp",
-  Discord = "com.hnc.Discord",
-  Telegram = "ru.keepcoder.Telegram",
-}
-
 hs.spoons.use("Contexts", {
   ---@type ContextsConfig
   config = {
