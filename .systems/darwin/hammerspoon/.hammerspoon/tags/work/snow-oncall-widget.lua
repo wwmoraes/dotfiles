cdc4d110c7487ce6e791c27ecca2c090983733e2
William Artero <github@artero.dev>
2022-06-28 20:50:32 +0200

    feat(hammerspoon): added work widgets for on-call and support calendars

---
+++ .systems/darwin/hammerspoon/.hammerspoon/tags/work/snow-oncall-widget.lua
@@ -0,0 +1,66 @@
+local logger = hs.logger.new("snow-onc", "error")
+local urls = require("helpers.urls")
+
+local snowURL = os.getenv("SNOW_URL")
+local groupID = os.getenv("SNOW_ONCALL_GROUP_ID")
+local rotasID = os.getenv("SNOW_ONCALL_ROTAS_ID")
+local rostersID = os.getenv("SNOW_ONCALL_ROSTERS_ID")
+local timeZone = hs.settings.get("timeZone") or "Universal"
+
+logger.i("building URL...")
+local url = urls.build(snowURL, "$oc.do", {
+  ["sysparm_include_view"] = "daily,weekly,monthly",
+  ["sysparm_timezone"] = timeZone,
+  ["sysparm_timeline_enabled"] = "true",
+  ["sysparm_current_view"] = "weekly",
+  ["sysparm_group_id"] = groupID,
+  ["sysparm_rotas"] = rotasID,
+  ["sysparm_rosters"] = rostersID,
+  ["sysparm_show_gaps"] = "false",
+  ["sysparm_show_conflicts"] = "false"
+})
+
+logger.i("creating WebView instance...")
+local webView = hs.webview.new(hs.geometry.rect(348, 52, 860, 236)):sendToBack()
+webView:behavior(hs.drawing.windowBehaviors.canJoinAllSpaces)
+
+logger.i("configuring navigation callback...")
+webView:navigationCallback(function(action, view)
+  if action ~= "didFinishNavigation" then return end
+
+  view:evaluateJavaScript([[
+    const stylesElem = document.createElement("style");
+    stylesElem.type = "text/css";
+    stylesElem.innerText = `
+    /* reposition progress indicator */
+    .icon-loading { top: 9px !important; left: 9px !important; right: auto !important; }
+
+    /* remove unneeded controls */
+    .dhx_cal_navline, sn-on-call-filter { display: none !important; }
+
+    /* fix header after control removal */
+    .dhx_cal_header { top: 0px !important; left: -2px !important; }
+    .dhx_cal_data { top: 36px !important; overflow: hidden; height: 440px !important; }
+
+    /* hide group rows (roster + time off) */
+    .dhx_cal_data .dhx_row_folder { display: none; }
+
+    /* hide time off rows */
+    .dhx_cal_data .dhx_row_folder:not(:first-of-type) ~ .dhx_row_item { display: none; }
+    `.trim();
+    document.head.appendChild(stylesElem);
+    true;
+  ]], function(result, error)
+    if result ~= nil then logger.i("JS result:", result) end
+    if error ~= nil and error.code ~= 0 then
+      logger.e("JS error:", hs.inspect(error))
+    end
+  end)
+
+  view:show()
+end)
+
+logger.i("loading URL...")
+webView:url(url)
+
+return webView
---
+++ .systems/darwin/hammerspoon/.hammerspoon/tags/work/snow-support-widget.lua
@@ -0,0 +1,66 @@
+local logger = hs.logger.new("snow-sup", "error")
+local urls = require("helpers.urls")
+
+local snowURL = os.getenv("SNOW_URL")
+local groupID = os.getenv("SNOW_SUPPORT_GROUP_ID")
+local rotasID = os.getenv("SNOW_SUPPORT_ROTAS_ID")
+local timeZone = hs.settings.get("timeZone") or "Universal"
+
+logger.i("building URL...")
+local url = urls.build(snowURL, "$oc.do", {
+  ["sysparm_include_view"] = "daily,weekly,monthly",
+  ["sysparm_timezone"] = timeZone,
+  ["sysparm_timeline_enabled"] = "false",
+  ["sysparm_current_view"] = "daily",
+  ["sysparm_group_id"] = groupID,
+  ["sysparm_rotas"] = rotasID,
+  ["sysparm_show_gaps"] = "false",
+  ["sysparm_show_conflicts"] = "false",
+  ["sysparm_rosters"] = "",
+})
+
+logger.i("creating WebView instance...")
+local webView = hs.webview.new(hs.geometry.rect(24, 52, 300, 486)):sendToBack()
+webView:behavior(hs.drawing.windowBehaviors.canJoinAllSpaces)
+
+logger.i("configuring navigation callback...")
+webView:navigationCallback(function(action, view)
+  if action ~= "didFinishNavigation" then return end
+
+  view:evaluateJavaScript([[
+    const stylesElem = document.createElement("style");
+    stylesElem.type = "text/css";
+    stylesElem.innerText = `
+    /* reposition progress indicator */
+    .ing-indicator.icon-loading { top: 11px !important; right: 11px !important; }
+
+    /* remove unneeded controls */
+    .calendarHeader .leftContainer,
+    .calendarHeader .rightContainer,
+    sn-on-call-filter
+    { display: none !important; }
+
+    /* fix header after control removal */
+    .calendarHeader .middleContainer { margin-top: 0 !important; }
+    .dhx_cal_navline { height: 44px !important; margin-bottom: 0; border: 0; }
+    .dhx_cal_data { top: 44px !important; overflow: hidden; height: 440px !important; }
+
+    /* reposition calendar view */
+    .dhx_scale_holder, .dhx_scale_holder_now { top: -352px !important; } /* each hour = 44px */
+    `.trim();
+    document.head.appendChild(stylesElem);
+    true;
+  ]], function(result, error)
+    if result ~= nil then logger.i("JS result:", result) end
+    if error ~= nil and error.code ~= 0 then
+      logger.e("JS error:", hs.inspect(error))
+    end
+  end)
+
+  view:show()
+end)
+
+logger.i("loading URL...")
+webView:url(url)
+
+return webView
