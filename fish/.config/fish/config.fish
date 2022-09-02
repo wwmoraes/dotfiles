e6a95d4436eee4f49a61829f1d1b02307eb63574
William Artero <github@artero.dev>
2022-09-02 21:26:40 +0200

    chore: kitty+fish integration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,13 @@
 # Remove the global version, as it shadows the universal one
 set -eg fish_user_paths
 
+# manual kitty integration
+if set -q KITTY_INSTALLATION_DIR
+  set --global KITTY_SHELL_INTEGRATION enabled
+  source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
+  set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
+end
+
 # try to launch tmux
 launch-tmux
 
