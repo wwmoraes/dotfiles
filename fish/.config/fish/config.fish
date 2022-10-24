ddd0a3d52a6e7229b10cb7764f359c978ae90230
William Artero <github@artero.dev>
2022-10-24 15:51:43 +0200

    feat(fish): VSCode integration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,6 +8,11 @@ if set -q KITTY_INSTALLATION_DIR
   set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
 end
 
+# manual code integration
+set -l CODE (command -v code | command -v code-oss | command -v codium)
+string match -q "$TERM_PROGRAM" "vscode"
+and . ($CODE --locate-shell-integration-path fish)
+
 # try to launch tmux
 launch-tmux
 
