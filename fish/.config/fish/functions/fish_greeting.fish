f80ac4ad68470e88f536f8a68d5eacb89fa5561e
William Artero <github@artero.dev>
2022-11-09 17:33:11 +0100

    chore(fish): ignore errors on prompt message curl

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,4 +1,4 @@
 function fish_greeting
   type -q doge; and ls | doge
-  curl -fs --max-time 0.3 http://whatthecommit.com/index.txt
+  curl -fs --max-time 0.3 https://whatthecommit.com/index.txt || true
 end
