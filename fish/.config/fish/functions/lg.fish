4533735ee71849d12a2608776d454c74a0859a0c
William Artero <git@artero.dev>
2023-09-08 15:31:43 +0200

    refactor(fish): simplified opening lazygit on a target path

--- fish/.config/fish/functions/lg.fish
+++ fish/.config/fish/functions/lg.fish
@@ -4,15 +4,10 @@ function lg -a path -d "Launch lazygit on current (or given) directory"
     return 1
   end
 
-  if test "$path" != ""; emit lg_open $path; end
-  lazygit
-  if test "$path" != ""; emit lg_close; end
+  test "$path" != ""; or set -l path $PWD
+
+  pgpz unlock > /dev/null &
+  lazygit -p "$path"
 end
 
-function _lg_pushd -a path --on-event lg_open
-  pushd "$path" > /dev/null
-end
-
-function _lg_popd --on-event lg_close
-  popd > /dev/null
-end
+functions -e _lg_pushd _lg_popd
