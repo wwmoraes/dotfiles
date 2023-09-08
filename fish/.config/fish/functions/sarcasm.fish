17919c15a64ae46a978fa054d2ed66fcde41edb9
William Artero <github@artero.dev>
2023-09-08 14:48:22 +0200

    feat(fish): sarcasm command, because I'm lazy even for that.

---
+++ fish/.config/fish/functions/sarcasm.fish
@@ -0,0 +1,31 @@
+function sarcasm -d "transforms text into the supreme form"
+  argparse 'i/invert' -- $argv[2..-1]
+
+  set -l casing (test (string length -- "$_flag_invert") -gt 0; echo $status)
+
+  set content "$argv[2..-1]"
+  # use stdin if no args were given
+  string match -qa "" "$content"; and read content
+
+  string split '' "$content" | while read char
+    # passthrough non-alpha characters
+    string match -qr "[[:alpha:]]" $char; or begin
+      echo $char
+      continue
+    end
+
+    # transform alpha
+    switch "$casing"
+      case "0"
+        string lower $char
+      case "1"
+        string upper $char
+      case "" "*"
+        echo "unknown casing state" > /dev/stderr
+        return 1
+    end
+
+    # set the casing for the next character
+    set casing (math bitand 1, $casing+1)
+  end | string join ''
+end
