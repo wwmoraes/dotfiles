09964c327c737beb809b24e86f6b5934cdd49aaa
William Artero <git@artero.dev>
2023-10-13 14:14:40 +0200

    fix(fish): use homebrew prefix on greeting

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,6 +1,8 @@
 command -q fortune; or functions -e fish_greeting && exit
 command -q cowsay; or functions -e fish_greeting && exit
 
+set -q HOMEBREW_PREFIX; or set -l HOMEBREW_PREFIX (realpath (dirname (which brew))/..)
+
 function fish_greeting
-  fortune | cowsay -n -W 80 -f (ls /opt/homebrew/share/cows/*.cow | shuf -n1)
+  fortune | cowsay -n -W 80 -f (ls $HOMEBREW_PREFIX/share/cows/*.cow | shuf -n1)
 end
