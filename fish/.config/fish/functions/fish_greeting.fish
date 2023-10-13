42be63b182e90879c9cd77d4528c6837b3f070ed
William Artero <git@artero.dev>
2023-10-13 13:42:04 +0200

    chore(fish): added safety checks on greeting for a graceful fallback

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,3 +1,6 @@
+command -q fortune; or functions -e fish_greeting && exit
+command -q cowsay; or functions -e fish_greeting && exit
+
 function fish_greeting
   fortune | cowsay -n -W 80 -f (ls /opt/homebrew/share/cows/*.cow | shuf -n1)
 end
