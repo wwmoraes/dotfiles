416f1ee956b75ee91f7863fc79c76fce454ecd78
William Artero <git@artero.dev>
2023-10-18 18:40:04 +0200

    chore(fish): removed pgpz unlock

--- .shadow.d/.config/fish/config.fish
+++ .shadow.d/.config/fish/config.fish
@@ -1,9 +1,5 @@
 # Remove the global version, as it shadows the universal one
 set -eg fish_user_paths
 
-pgpz unlock > /dev/null &
-
-# source ~/.config/fish/functions/fish_prompt.fish &
-
 # try to launch tmux
 launch-tmux
