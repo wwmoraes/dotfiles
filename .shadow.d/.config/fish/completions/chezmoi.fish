792ce374f042e5d8f8068356a47e877698c9d4f1
William Artero <git@artero.dev>
2023-10-27 23:55:43 +0200

    feat: chezmoi env, a command to update env vars

--- .shadow.d/.config/fish/completions/chezmoi.fish
+++ .shadow.d/.config/fish/completions/chezmoi.fish
@@ -1,5 +1,6 @@
 source (brew --prefix)/share/fish/vendor_completions.d/chezmoi.fish
 
 complete -xc chezmoi -n '__fish_use_subcommand' -a check -d "Dry-run report of package changes"
-complete -xc chezmoi -n '__fish_use_subcommand' -a lg -d "Open lazygit at the chezmoi source path"
-complete -xc chezmoi -n '__fish_use_subcommand' -a sync -d "Re-applies the brew bundle"
+complete -xc chezmoi -n '__fish_use_subcommand' -a lg -d "Open lazygit in the chezmoi source path"
+complete -xc chezmoi -n '__fish_use_subcommand' -a sync -d "Updates environment with the latest brew bundle settings"
+complete -xc chezmoi -n '__fish_use_subcommand' -a env -d "Updates environment variables"
