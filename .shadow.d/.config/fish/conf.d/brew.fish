1611bd550d607407d4ad86e18d7490bf08dce698
William Artero <git@artero.dev>
2023-11-08 15:56:37 +0100

    chore(fish): safe brew loading

--- .shadow.d/.config/fish/conf.d/brew.fish
+++ .shadow.d/.config/fish/conf.d/brew.fish
@@ -1,6 +1,7 @@
-if test (uname -m) = "arm64"
+if test (uname -s) = "Darwin" && test (uname -m) = "arm64"
   eval (/opt/homebrew/bin/brew shellenv)
   alias brew="arch -arm64 /opt/homebrew/bin/brew"
 else
-  eval (/usr/local/Homebrew/bin/brew shellenv)
+  # skip if brew is not found
+  command -q brew; and eval (brew shellenv)
 end
