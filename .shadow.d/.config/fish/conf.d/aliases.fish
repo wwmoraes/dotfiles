8453c410bee0123f3c49c08d11b107cd902f02f3
William Artero <git@artero.dev>
2023-11-08 16:03:34 +0100

    fix(fish): safer abbr bt checking the dependencies

--- .shadow.d/.config/fish/conf.d/aliases.fish
+++ .shadow.d/.config/fish/conf.d/aliases.fish
@@ -1,7 +1,12 @@
 # only apply on interactive shells
 status --is-interactive; or exit
 
-alias doctl="op plugin run -- doctl"
-alias gh="op plugin run -- gh"
-alias hub="op plugin run -- hub"
-alias ls="grc ls -CF --color"
+if command -q grc
+  alias ls="grc ls -CF --color"
+end
+
+if command -q op
+  alias doctl="op plugin run -- doctl"
+  alias gh="op plugin run -- gh"
+  alias hub="op plugin run -- hub"
+end
