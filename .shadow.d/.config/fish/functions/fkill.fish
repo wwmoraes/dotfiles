0a51235d4bffc483aff801a970a21df255cd7b1f
William Artero <git@artero.dev>
2023-11-20 14:12:20 +0100

    chore(fish): shorter commands on fkill
    Signed-off-by: William Artero <git@artero.dev>

--- .shadow.d/.config/fish/functions/fkill.fish
+++ .shadow.d/.config/fish/functions/fkill.fish
@@ -1,5 +1,5 @@
 function fkill -d "Fuzzy kill"
-  set pid (ps -o pid,user,command | sed 1d | fzf -m | awk '{print $1}')
+  set pid (ps axww -o pid,user,comm | sed 1d | fzf -m | awk '{print $1}')
 
   if test -n "$pid"
     echo $pid | xargs kill $argv
