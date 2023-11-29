49aab67d2e09d551ee00ae2fc9a5dc243c3ababb
William Artero <git@artero.dev>
2023-11-29 19:42:41 +0100

    fix(fish): k8s finalize-namespace now shuts down the proxy correctly
    Signed-off-by: William Artero <git@artero.dev>

--- .shadow.d/.config/fish/functions/k8s.fish
+++ .shadow.d/.config/fish/functions/k8s.fish
@@ -1,22 +1,22 @@
 function k8s -a cmd -d "utilities for Kubernetes management"
   switch "$cmd"
   # TODO create kubectl plugin
-  case "finalize-namespace"
-    set -l NAMESPACE (kubectl get namespaces \
+  case finalize-namespace
+    set -l resource (kubectl get namespaces \
       | fzf --ansi --header-lines=1 \
       | awk '{print $1}')
 
-    set -l PATCH_FILE (mktemp)
-    kubectl get namespace $NAMESPACE -o json | jq '.spec.finalizers = []' > $PATCH_FILE
+    set -l patch_file (mktemp)
+    kubectl get namespace $resource -o json | jq '.spec.finalizers = []' > $patch_file
 
     kubectl proxy &
     set -l pid $last_pid
     sleep 1
 
-    curl -k -H "Content-Type: application/json" -X PUT --data-binary @$PATCH_FILE 127.0.0.1:8001/api/v1/namespaces/$NAMESPACE/finalize || true
-    kill $sudo_pid 2> /dev/null
+    curl -k -H "Content-Type: application/json" -X PUT --data-binary @$patch_file 127.0.0.1:8001/api/v1/namespaces/$resource/finalize || true
+    kill $last_pid 2> /dev/null
 
-    rm -f $PATCH_FILE
+    rm -f $patch_file
   end
 end
 
