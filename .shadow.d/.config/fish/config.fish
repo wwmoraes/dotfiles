42c456af9f432b0b9e7b71c8f56bf9e5abb23100
William Artero <git@artero.dev>
2023-12-06 15:10:59 +0100

    fix(fish): use universal PATH if set

--- .shadow.d/.config/fish/config.fish
+++ .shadow.d/.config/fish/config.fish
@@ -2,5 +2,8 @@
 set -eg fish_user_paths
 set -eg SSH_AUTH_SOCK
 
+# safely erase the inherited global PATH and use the universal one
+set -qU PATH; and set -eg PATH
+
 # try to launch tmux
 launch-tmux
