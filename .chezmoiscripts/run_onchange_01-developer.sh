263d7d3bc93e590751fcd2d7a9ad84020eb2c7af
William Artero <git@artero.dev>
2024-03-29 19:17:12 +0100

    feat: added taskport privilege setting for Darwin environments

--- .chezmoiscripts/run_onchange_01-developer.sh
+++ .chezmoiscripts/run_onchange_01-developer.sh
@@ -22,3 +22,6 @@ if ! _=$(groups | xargs -n1 | grep -Fx _developer > /dev/null); then
   echo "adding user to group _developer"
   sudo dscl . append /Groups/_developer GroupMembership "$(whoami)"
 fi
+
+echo "configuring taskport privilege"
+security authorizationdb write system.privilege.taskport allow
