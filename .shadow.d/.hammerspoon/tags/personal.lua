1c03338d7e64b03948de986e2f69dbb762a8d383
William Artero <git@artero.dev>
2024-07-11 10:29:25 +0200

    chore: removing unused tokens from env
    Signed-off-by: William Artero <git@artero.dev>

--- .shadow.d/.hammerspoon/tags/personal.lua
+++ .shadow.d/.hammerspoon/tags/personal.lua
@@ -3,18 +3,3 @@
 local logger = hs.logger.new("personal", "verbose")
 
 logger.i("loading personal tag settings")
-
-local linkIPURL = os.getenv("NEXTDNS_LINK_IP_URL")
-
-if linkIPURL == nil or linkIPURL:len() <= 0 then
-  logger.w("NEXTDNS_LINK_IP_URL unset or empty, skipping reachability callback")
-else
-  hs.network.reachability.forAddress("172.24.174.4"):setCallback(function(self, flags)
-    if (flags & hs.network.reachability.flags.isLocalAddress) > 0 then
-      logger.i("VPN is up!")
-      hs.http.get(linkIPURL, nil)
-    else
-      logger.i("VPN is down!")
-    end
-  end):start()
-end
