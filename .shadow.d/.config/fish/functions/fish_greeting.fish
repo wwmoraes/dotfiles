c9dff2444bcc5eaffc808ea538cce8b68c8da1b7
William Artero <git@artero.dev>
2024-08-01 17:13:00 +0200

    refactor: sayonara Brewfile
    Finally, nix-managed brews!

--- .shadow.d/.config/fish/functions/fish_greeting.fish
+++ .shadow.d/.config/fish/functions/fish_greeting.fish
@@ -2,5 +2,5 @@ command -q fortune; or exit
 command -q cowsay; or exit
 
 function fish_greeting
-  fortune | cowsay -n -W 80 -f (ls /opt/homebrew/share/cows/*.cow | shuf -n1)
+  fortune | cowsay -n -W 80 --random
 end
