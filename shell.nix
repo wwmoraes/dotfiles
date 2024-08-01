d53e5794fbbc380f4e4b479d0522520f03d32a0c
William Artero <git@artero.dev>
2024-08-02 01:42:50 +0200

    chore: added ejson and commitlint to shell
    Signed-off-by: William Artero <git@artero.dev>

--- shell.nix
+++ shell.nix
@@ -4,26 +4,33 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
     sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
   };
+  pkgs = import nixpkgs {};
   nixpkgs-unstable = fetchTarball {
     name = "nixos-unstable-a14c5d651cee9ed70f9cd9e83f323f1e531002db";
     # url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixpkgs-unstable.tar.gz";
     url = "https://github.com/NixOS/nixpkgs/archive/a14c5d651cee9ed70f9cd9e83f323f1e531002db.tar.gz";
     sha256 = "1b2dwbqm5vdr7rmxbj5ngrxm7sj5r725rqy60vnlirbbwks6aahb";
   };
-  pkgs = import nixpkgs {};
   unstable = import nixpkgs-unstable {};
+  kaizen-src = fetchTarball {
+    name = "kaizen-2dc5f25534649dcf7450fd8ea45413f7d7f92866";
+    url = "https://github.com/wwmoraes/kaizen/archive/2dc5f25534649dcf7450fd8ea45413f7d7f92866.tar.gz";
+    sha256 = "07cc53yqpsilc6mvb2jxgbjb80dafnl7n0my5wswg706rspimvr2";
+  };
+  kaizen = import kaizen-src { inherit pkgs; };
   inherit (pkgs) mkShell;
 in mkShell {
   packages = with pkgs; [
+    # unstable._1password
     bash
     chezmoi
     editorconfig-checker
-    # ejson
     fish
-    # git
+    git
+    kaizen.ejson
+    kaizen.go-commitlint
     markdownlint-cli
     shellcheck
-    # unstable._1password
     unstable.lazygit
     yamllint
   ];
