28282f873febf781c251b4867a920527d6dd5a9d
William Artero <git@artero.dev>
2024-08-05 23:05:02 +0200

    chore: updated chezmoi plugins to use direnv
    Signed-off-by: William Artero <git@artero.dev>

--- .shadow.d/.config/fish/completions/chezmoi.fish
+++ .shadow.d/.config/fish/completions/chezmoi.fish
@@ -6,10 +6,8 @@ function __chezmoi_scripts
   printf "%s\tChezmoi Script\n" (chezmoi managed ~/.chezmoiscripts/ | xargs -n1 basename)
 end
 
-complete -xc chezmoi -n '__fish_use_subcommand' -a check -d "Dry-run report of package changes"
 complete -xc chezmoi -n '__fish_use_subcommand' -a env -d "Updates environment variables"
 complete -xc chezmoi -n '__fish_use_subcommand' -a lg -d "Open lazygit in the chezmoi source path"
 complete -xc chezmoi -n '__fish_use_subcommand' -a run -d "Execute a script"
-complete -xc chezmoi -n '__fish_use_subcommand' -a 'switch' -d "Builds and activates the latest nix profile settings"
 complete -xc chezmoi -n '__fish_use_subcommand' -a sync -d "Updates environment with the latest brew bundle settings"
 complete -xc chezmoi -n '__fish_seen_subcommand_from run' -a "(__chezmoi_scripts)"
