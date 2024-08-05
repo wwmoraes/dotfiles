51987b166c018172124965d0244aaa168d3228d8
William Artero <git@artero.dev>
2024-08-05 23:56:40 +0200

    chore(fish): always load nix env
    Signed-off-by: William Artero <git@artero.dev>

--- .shadow.d/.config/fish/conf.d/nix.fish
+++ .shadow.d/.config/fish/conf.d/nix.fish
@@ -7,4 +7,4 @@ functions -e add_path
 test -e /nix/var/nix/profiles/default/etc/profile.d/nix.fish;
 and source /nix/var/nix/profiles/default/etc/profile.d/nix.fish
 
-source /etc/fish/nixos-env-preinit.fish
+source /etc/fish/setEnvironment.fish
