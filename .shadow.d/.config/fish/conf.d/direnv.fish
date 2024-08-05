eb16a2616ca9ec837b294244d4948035114dafad
William Artero <git@artero.dev>
2024-08-05 16:10:10 +0200

    chore: cleanup settings and leftovers
    Signed-off-by: William Artero <git@artero.dev>

--- .shadow.d/.config/fish/conf.d/direnv.fish
+++ .shadow.d/.config/fish/conf.d/direnv.fish
@@ -1,15 +1,3 @@
 command -q direnv; or exit
 
-## why developers still suggest globals on fish? :P
-set -eg direnv_fish_mode
-
-## trigger direnv at prompt, and on every arrow-based directory change (default)
-set -U direnv_fish_mode eval_on_arrow
-
-## trigger direnv at prompt, and only after arrow-based directory changes before executing command
-# set -U direnv_fish_mode eval_after_arrow
-
-## trigger direnv at prompt only, this is similar functionality to the original behavior
-# set -U direnv_fish_mode disable_arrow
-
 direnv hook fish | source
--- .shadow.d/.config/fish/conf.d/nix.fish
+++ .shadow.d/.config/fish/conf.d/nix.fish
@@ -1,6 +1,5 @@
-# don't setup anything if nix is not installed
-command -q nix; or exit
+test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish';
+and source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
 
-test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'; or exit
-
-source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
+test -e '/nix/var/nix/profiles/default/etc/profile.d/nix.fish';
+and source '/nix/var/nix/profiles/default/etc/profile.d/nix.fish'
