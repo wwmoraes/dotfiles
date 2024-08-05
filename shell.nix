8eb94940d66ee2dbdc548aef58887fa4a17eb9f7
William Artero <git@artero.dev>
2024-08-05 12:26:01 +0200

    chore: updated nix shell
    Signed-off-by: William Artero <git@artero.dev>

--- shell.nix
+++ shell.nix
@@ -1,23 +1,19 @@
 let
   ## nix-prefetch-url --unpack <url>
-  nixpkgs = fetchTarball {
+  pkgs = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
     sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
-  };
-  pkgs = import nixpkgs {};
-  nixpkgs-unstable = fetchTarball {
+  }) {};
+  unstable = import (fetchTarball {
     name = "nixos-unstable-a14c5d651cee9ed70f9cd9e83f323f1e531002db";
-    # url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixpkgs-unstable.tar.gz";
     url = "https://github.com/NixOS/nixpkgs/archive/a14c5d651cee9ed70f9cd9e83f323f1e531002db.tar.gz";
     sha256 = "1b2dwbqm5vdr7rmxbj5ngrxm7sj5r725rqy60vnlirbbwks6aahb";
-  };
-  unstable = import nixpkgs-unstable {};
-  kaizen-src = fetchTarball {
-    name = "kaizen-2dc5f25534649dcf7450fd8ea45413f7d7f92866";
-    url = "https://github.com/wwmoraes/kaizen/archive/2dc5f25534649dcf7450fd8ea45413f7d7f92866.tar.gz";
-    sha256 = "07cc53yqpsilc6mvb2jxgbjb80dafnl7n0my5wswg706rspimvr2";
-  };
-  kaizen = import kaizen-src { inherit pkgs; };
+  }) {};
+  kaizen = import (fetchTarball {
+    name = "kaizen-8075b45edf93d8f95a00958fd3a1cc606ba3405c";
+    url = "https://github.com/wwmoraes/kaizen/archive/8075b45edf93d8f95a00958fd3a1cc606ba3405c.tar.gz";
+    sha256 = "1w6fkd5kqm2l2aij31kd9ddk84phbcir7flrbha6mxw2y4j36z09";
+  }) { inherit pkgs; };
   inherit (pkgs) mkShell;
 in mkShell {
   packages = with pkgs; [
@@ -27,6 +23,7 @@ in mkShell {
     editorconfig-checker
     fish
     git
+    go-task
     kaizen.ejson
     kaizen.go-commitlint
     markdownlint-cli
