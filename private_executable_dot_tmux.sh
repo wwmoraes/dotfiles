5b9ae2959a296f11368acf0b9a6998beeb0047dc
William Artero <git@artero.dev>
2024-08-07 03:08:48 +0200

    refactor: nix-managed tmux
    Signed-off-by: William Artero <git@artero.dev>

--- private_executable_dot_tmux.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# command -v powerline >/dev/null 2>&1 || exit 0
-# command -v powerline-daemon >/dev/null 2>&1 || exit 0
-# command -v powerline-config >/dev/null 2>&1 || exit 0
-# command -v powerline-render >/dev/null 2>&1 || exit 0
-
-# powerline-daemon -q
-# powerline-config tmux setup
