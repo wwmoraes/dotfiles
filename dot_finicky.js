44823c54c4eb5d6b470e7a00fa5da1458c146870
William Artero <git@artero.dev>
2024-08-16 11:45:23 +0200

    feat(finicky): unwrap mandrill links
    Signed-off-by: William Artero <git@artero.dev>

--- dot_finicky.js
+++ dot_finicky.js
@@ -196,6 +196,35 @@ if (typeof URLSearchParams === "undefined" || URLSearchParams === null) {
   };
 }
 
+if (typeof atob === "undefined" || atob === null) {
+  const b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+        // Regular expression to check formal correctness of base64 encoded strings
+        b64re = /^(?:[A-Za-z\d+\/]{4})*?(?:[A-Za-z\d+\/]{2}(?:==)?|[A-Za-z\d+\/]{3}=?)?$/;
+
+  const atob = function(string) {
+    // atob can work with strings with whitespaces, even inside the encoded part,
+    // but only \t, \n, \f, \r and ' ', which can be stripped.
+    string = String(string).replace(/[\t\n\f\r ]+/g, "");
+    if (!b64re.test(string))
+        throw new TypeError("Failed to execute 'atob' on 'Window': The string to be decoded is not correctly encoded.");
+
+    // Adding the padding if missing, for semplicity
+    string += "==".slice(2 - (string.length & 3));
+    var bitmap, result = "", r1, r2, i = 0;
+    for (; i < string.length;) {
+        bitmap = b64.indexOf(string.charAt(i++)) << 18 | b64.indexOf(string.charAt(i++)) << 12
+                | (r1 = b64.indexOf(string.charAt(i++))) << 6 | (r2 = b64.indexOf(string.charAt(i++)));
+
+        result += r1 === 64 ? String.fromCharCode(bitmap >> 16 & 255)
+                : r2 === 64 ? String.fromCharCode(bitmap >> 16 & 255, bitmap >> 8 & 255)
+                : String.fromCharCode(bitmap >> 16 & 255, bitmap >> 8 & 255, bitmap & 255);
+    }
+    return result;
+  };
+
+  globalThis.atob = atob;
+}
+
 /** @type {Applications} */
 const apps = {
   Chrome: "com.google.Chrome",
@@ -263,6 +292,12 @@ const defaultPrefix = (prefix) => ({
   match: ({ urlString }) => !urlString.match(/^[a-z]+:\/\//),
   url: ({ urlString }) => "https://" + urlString,
 });
+
+/**
+ * @type {import("./.finicky.d").Finicky.URLFn}
+ */
+const fuckOffMandrill = ({ url }) =>
+  JSON.parse(JSON.parse(atob((new URLSearchParams(url.search)).get("p"))).p).url;
 
 /// <reference path="./.finicky.d.ts" />
 /** @type {import("./.finicky.d").Finicky.Config} */
@@ -365,6 +400,11 @@ module.exports = {
     {
       match: ({ url }) => url.host == "youtube.com",
       url: ({ url }) => "https://r.yattee.stream/watch?v=" + url.search.replace(/.*(v=[^&]+).*/, "$1"),
+    },
+    // Mandrill => wrapped URL
+    {
+      match: ({ urlString }) => urlString.startsWith("https://mandrillapp.com/track/click/"),
+      url: fuckOffMandrill,
     }
   ],
   handlers: [
