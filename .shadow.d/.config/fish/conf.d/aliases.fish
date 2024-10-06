fc201699a24914a21d992e6c07a27bef2a16a4e5
William Artero <git@artero.dev>
2024-10-06 14:25:05 +0200

    chore: added gh tool globally
    Signed-off-by: William Artero <git@artero.dev>

--- .shadow.d/.config/fish/conf.d/aliases.fish
+++ .shadow.d/.config/fish/conf.d/aliases.fish
@@ -8,6 +8,7 @@ end
 if command -q op
   command -q brew; and alias brew="op plugin run -- brew"
   command -q doctl; and alias doctl="op plugin run -- doctl"
+  command -q gh; and alias gh="op plugin run -- gh"
   command -q pulumi; and alias pulumi="op plugin run -- pulumi"
 end
 
