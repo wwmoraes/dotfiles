020e18fbf898490abdbd0214c5629b06c7056783
William Artero <git@artero.dev>
2024-11-02 17:23:31 +0100

    fix(fish): autocomplete reload hooks
    Signed-off-by: William Artero <git@artero.dev>

--- .shadow.d/.config/fish/conf.d/xdg.fish
+++ .shadow.d/.config/fish/conf.d/xdg.fish
@@ -1,8 +1,8 @@
 function __reload_completions --on-variable XDG_DATA_DIRS
 	for dir in (string split ":" $XDG_DATA_DIRS)
-		test -d $dir/fish/vendor_completions.d
-		or continue
+		test -d $dir/fish/vendor_completions.d; or continue
 
-		source $dir/fish/vendor_completions.d/*.fish
+		set -l files $dir/fish/vendor_completions.d/*.fish
+		count $files > /dev/null; and source $files
 	end
 end
