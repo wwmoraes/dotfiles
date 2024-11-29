295ecac91bf998d3e8e1018fc11aa40279ff3eb3
William Artero <git@artero.dev>
2024-11-29 09:46:04 +0100

    chore: update work functions and env

--- .shadow.d/.config/fish/functions/work.fish
+++ .shadow.d/.config/fish/functions/work.fish
@@ -2,14 +2,14 @@ function work -a cmd -d "work utilities so I can stay productive"
   switch "$cmd"
     case start
       set -l daemons (launchctl list | awk '$1 != "-" {print}')
-      for f in /Library/LaunchAgents/*
+      for f in /Library/LaunchAgents/{com.jamf.,com.zscaler.}*.plist
         set -l label (/usr/libexec/PlistBuddy -c 'Print Label' $f)
         echo $daemons | grep -qF $label; and continue
 
         echo "loading $label"
         launchctl load -w $f 2> /dev/null
       end
-      for f in /Library/LaunchDaemons/*
+      for f in /Library/LaunchDaemons/{com.jamf.,com.zscaler.,com.snowsoftware.}*.plist
         set -l label (/usr/libexec/PlistBuddy -c 'Print Label' $f)
         echo $daemons | grep -qF $label; and continue
 
@@ -38,16 +38,39 @@ function work -a cmd -d "work utilities so I can stay productive"
         echo "opening $app"
         open -a "$app"
       end
+    case stop
+      for f in /Library/Launch{Agents,Daemons}/*.plist
+        set -l label (/usr/libexec/PlistBuddy -c 'Print Label' $f)
+        echo "stopping $label"
+        sudo launchctl bootout "system/$label"
+      end
     case elevate
       sudo dseditgroup -o edit -a (whoami) -t user admin
     case demote
       sudo dseditgroup -o edit -d (whoami) -t user admin
     case groups
       az ad user get-member-groups --id $EMAIL --query [].displayName -o tsv
+    case update
+      for f in /Library/LaunchDaemons/{com.jamf.,com.zscaler.}*.plist
+        set -l label (/usr/libexec/PlistBuddy -c 'Print Label' $f)
+        echo $daemons | grep -qF $label; and continue
+
+        echo "loading $label"
+        launchctl load -w $f 2> /dev/null
+      end
+      sudo softwareupdate --install --all --force --restart --agree-to-license
     case "*"
       echo "work: unknown command '$cmd'"
   end
 end
 
-complete -xc work -n __fish_use_subcommand -a elevate -d "adds the current user account to the admin group"
-complete -xc work -n __fish_use_subcommand -a demote -d "removes the current user account from the admin group"
+complete -xc work -n __fish_use_subcommand -a elevate \
+  -d "adds the current user account to the admin group"
+complete -xc work -n __fish_use_subcommand -a demote \
+  -d "removes the current user account from the admin group"
+complete -xc work -n __fish_use_subcommand -a start \
+  -d "bootstraps launch daemons, agents and login items in safe boot mode"
+complete -xc work -n __fish_use_subcommand -a update \
+  -d "applies the OS software updates"
+complete -xc work -n __fish_use_subcommand -a groups \
+  -d "lists AD groups for the principal in EMAIL"
