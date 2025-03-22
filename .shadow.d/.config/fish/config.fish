c38f72efefa8290109d5ecb1d9a8e0b56b46706c
William Artero <git@artero.dev>
2025-03-22 16:17:25 +0100

    chore: sayonara tmux
    Zellij is for months my daily terminal multiplexer solution.
    
    Signed-off-by: William Artero <git@artero.dev>

--- .shadow.d/.config/fish/config.fish
+++ .shadow.d/.config/fish/config.fish
@@ -7,67 +7,34 @@ and source /etc/fish/config.fish
 # configure TTY tab stops
 command -q tabs; and tabs -2
 
-# not a tty session
+# skip non-tty sessions
 tty -s; or return
 test -t 0; or return
 
-# non-login session
+# skip non-login sessions
 status --is-login; or return
 
-# non-interactive session
+# skip non-interactive sessions
 status --is-interactive; or return
 
 command -q direnv
 and test -e .envrc
 and direnv reload
 
-# Apple Terminal.app
+# skip if in Apple Terminal.app
 string match -q "Apple_Terminal" $TERM_PROGRAM; and return
 
-# inside a tmux session
+# skip if inside a tmux/GNU screen session
 string match -q "screen*" $TERM; and return
 
-# inside a zellij session
+# skip if inside a zellij session
 set -Sq ZELLIJ; and return
 
-echo checking tmux
-if test -n "$USE_TMUX"
-	# tmux not found
-	command -v tmux > /dev/null; or return
+# zellij not found
+command -v zellij > /dev/null; or return
 
-	# get active sessions + "defaults"
-	set -l activeTmuxSessions (tmux list-sessions -F '#S' 2>/dev/null)
-	set -l tmuxSessions (cat "$HOME/.tmux.sessions.conf" 2>/dev/null)
-	set -l smugSessions (ls -1 $HOME/.config/smug/*.yml 2>/dev/null | ifne xargs basename -s .yml)
-	set -l sessions (string join \n $activeTmuxSessions $tmuxSessions $smugSessions | sort -u | awk NF)
+# remove dead session as zellij fails to ressurrect it
+zellij list-sessions --no-formatting | rg "^main\b"
+or zellij delete-session main
 
-	# offer options
-	if command -v fzf > /dev/null
-	  set session (string join \n $sessions | fzf --print-query --reverse -0 | tail -n1)
-	else
-	  echo "tmux sessions: "(echo $sessions | xargs)
-	  echo -n "tmux session name: "
-	  read session
-	end
-
-	# finally, execute tmux :D
-	test -n "$session"; or return
-
-	# create session using smug if it matches a template
-	command -v smug > /dev/null
-	and echo $activeTmuxSessions | not grep -qFx $session
-	and test -f "$HOME/.config/smug/$session.yml"
-	and smug "$session" -a --detach
-
-	# join or create plain session
-	exec tmux -u new -A -s "$session" > /dev/null
-else
-	# zellij not found
-	command -v zellij > /dev/null; or return
-	
-	# remove dead session as zellij fails to ressurrect it
-	zellij list-sessions --no-formatting | rg "^main\b"
-	or zellij delete-session main
-
-	exec zellij attach -c main
-end
+exec zellij attach -c main
