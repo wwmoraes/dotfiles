7d39fda2dbae0f5da815a381d22b514551ae1d87
William Artero <git@artero.dev>
2025-08-21 22:40:13 +0200

    refactor(finicky): migrate to v4 config & typedefs
    Signed-off-by: William Artero <git@artero.dev>
    ps-id: 435c7856-9260-47c5-9e38-488e3abbcd94

--- settings/common/darwin/programs/finicky/finicky.js
+++ settings/common/darwin/programs/finicky/finicky.js
@@ -1,228 +1,29 @@
-/** @typedef {"main"|"work"|"home"} Context */
-/** @typedef {Record<Context,string>} Contexts */
-/** @typedef {Record<string,Contexts>} Browsers */
-/** @typedef {Record<string,string>} Applications */
-
-if (typeof URLSearchParams === "undefined" || URLSearchParams === null) {
-	URLSearchParams = class URLSearchParamsPolyfill {
-		/**
-		 * @readonly
-		 * @type {number}
-		 */
-		size;
-
-		/**
-		 * @private
-		 * @type {Record<string, string[]>}
-		 */
-		data;
-
-		/**
-		 * @param {string[][] | Record<string, string> | string | URLSearchParams} [init]
-		 */
-		constructor(init) {
-			switch (typeof init) {
-				case "string":
-					Object.defineProperty(this, "data", {
-						configurable: false,
-						enumerable: true,
-						value: init.split("&").reduce(
-							/**
-							 * @param {Record<string,string[]>} obj
-							 */
-							(obj, entry) => {
-								const pair = entry.split("=", 2);
-								let value = obj[pair[0]] || new Array();
-								value.push(pair[1]);
-								obj[pair[0]] = value;
-								return obj;
-							},
-							{},
-						),
-						writable: false,
-					});
-					break;
-				default:
-					throw new Error("URLSearchParams polyfill supports string init only");
-			}
-
-			Object.defineProperty(this, "size", {
-				configurable: false,
-				enumerable: false,
-				value: Object.keys(this.data).length,
-				writable: false,
-			});
-		}
-
-		/**
-		 * Appends a specified key/value pair as a new search parameter.
-		 *
-		 * [MDN Reference](https://developer.mozilla.org/docs/Web/API/URLSearchParams/append)
-		 * @param {string} name
-		 * @param {string} value
-		 */
-		append(name, value) {
-			let currentValue = this.data[name] || new Array();
-			currentValue.push(value);
-			this.data[name] = currentValue;
-		}
-
-		/**
-		 * Deletes the given search parameter, and its associated value, from the list of all search parameters.
-		 *
-		 * [MDN Reference](https://developer.mozilla.org/docs/Web/API/URLSearchParams/delete)
-		 * @param {string} name
-		 * @param {string} [value]
-		 */
-		delete(name, value) {
-			const currentValues = this.data[name];
-			if (typeof currentValues === "undefined" || currentValues === null) {
-				return;
-			}
-
-			if (typeof value !== "undefined" && value !== null) {
-				this.data[name] = currentValues.filter(currentValue => currentValue != value);
-				return;
-			}
-
-			this.data[name] = null;
-		}
-
-		/**
-		 * Returns the first value associated to the given search parameter.
-		 *
-		 * [MDN Reference](https://developer.mozilla.org/docs/Web/API/URLSearchParams/get)
-		 * @param {string} name
-		 * @returns {string | null}
-		 */
-		get(name) {
-			return (this.data[name] || [])[0] || null;
-		};
-
-		/**
-		 * Returns all the values association with a given search parameter.
-		 *
-		 * [MDN Reference](https://developer.mozilla.org/docs/Web/API/URLSearchParams/getAll)
-		 * @param {string} name
-		 * @returns {string[]}
-		 */
-		getAll(name) {
-			return this.data[name] || [];
-		}
-
-		/**
-		 * Returns a Boolean indicating if such a search parameter exists.
-		 *
-		 * [MDN Reference](https://developer.mozilla.org/docs/Web/API/URLSearchParams/has)
-		 * @param {string} name
-		 * @param {string} [value]
-		 * @returns {boolean}
-		 */
-		has(name, value) {
-			const got = this.data[name];
-			if (typeof got !== "object" || typeof got[0] !== "string") {
-				return false;
-			}
-
-			if (typeof value !== "string") {
-				return true;
-			}
-
-			return got[0] == value;
-		}
-
-		/**
-		 * Sets the value associated to a given search parameter to the given value. If there were several values, delete the others.
-		 *
-		 * [MDN Reference](https://developer.mozilla.org/docs/Web/API/URLSearchParams/set)
-		 * @param {string} name
-		 * @param {string} value
-		 */
-		set(name, value) {
-			this.data[name] = [value];
-		}
-
-		/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/URLSearchParams/sort) */
-		sort() {
-
-		}
-
-		/** Returns a string containing a query string suitable for use in a URL. Does not include the question mark.
-		 * @returns {string}
-		 */
-		toString() {
-			return Object.entries(this.data).map(([key, values]) =>
-				values.map(value => key + "=" + value).join("&")).join("&");
-		}
-
-		/**
-		 * @callback forEachCallback
-		 * @param {string} value
-		 * @param {string} key
-		 * @param {URLSearchParams} parent
-		 */
-
-		/**
-		 * @param {forEachCallback} callbackfn
-		 * @param {any} [thisArg]
-		 */
-		forEach(callbackfn, thisArg) {
-			Object.entries(this.data).forEach(([key, values]) =>
-				values.forEach(value => callbackfn(value, key, thisArg || this)));
-		}
-
-		/** Returns an array of key, value pairs for every entry in the search params.
-		 * @returns {IterableIterator<[string, string]>}
-		 */
-		[Symbol.iterator]() {
-			return Object.entries(this.data).map(([key, values]) =>
-				values.map(value => [key, value])).flat(1);
-		}
-
-		/** Returns an array of key, value pairs for every entry in the search params.
-		 * @returns {IterableIterator<[string, string]>}
-		 */
-		entries() {
-			return Object.entries(this.data).map(([key, values]) =>
-				values.map(value => [key, value])).flat(1);
-		}
-
-		/** Returns a list of keys in the search params. */
-		/**
-		 * @returns {IterableIterator<string>}
-		 */
-		keys() {
-			return Object.keys(this.data);
-		}
-
-		/** Returns a list of values in the search params. */
-		/**
-		 * @returns {IterableIterator<string>}
-		 */
-		values() {
-			return Object.values(this.data);
-		}
-	};
-}
+/** @import {FinickyConfig, UrlTransformer, UrlRewriteRule} from "/Users/william/Applications/Finicky.app/Contents/Resources/finicky.d.ts" */
 
 if (typeof atob === "undefined" || atob === null) {
+	console.log("polyfilling atob");
+
 	const b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
 				// Regular expression to check formal correctness of base64 encoded strings
 				b64re = /^(?:[A-Za-z\d+\/]{4})*?(?:[A-Za-z\d+\/]{2}(?:==)?|[A-Za-z\d+\/]{3}=?)?$/;
 
-	const atob = function(string) {
+	/**
+		* @param {string} value
+		* @returns {string}
+	 */
+	const atob = function(value) {
 		// atob can work with strings with whitespaces, even inside the encoded part,
 		// but only \t, \n, \f, \r and ' ', which can be stripped.
-		string = String(string).replace(/[\t\n\f\r ]+/g, "");
-		if (!b64re.test(string))
+		value = String(value).replace(/[\t\n\f\r ]+/g, "");
+		if (!b64re.test(value))
 				throw new TypeError("Failed to execute 'atob' on 'Window': The string to be decoded is not correctly encoded.");
 
 		// Adding the padding if missing, for semplicity
-		string += "==".slice(2 - (string.length & 3));
+		value += "==".slice(2 - (value.length & 3));
 		var bitmap, result = "", r1, r2, i = 0;
-		for (; i < string.length;) {
-				bitmap = b64.indexOf(string.charAt(i++)) << 18 | b64.indexOf(string.charAt(i++)) << 12
-								| (r1 = b64.indexOf(string.charAt(i++))) << 6 | (r2 = b64.indexOf(string.charAt(i++)));
+		for (; i < value.length;) {
+				bitmap = b64.indexOf(value.charAt(i++)) << 18 | b64.indexOf(value.charAt(i++)) << 12
+								| (r1 = b64.indexOf(value.charAt(i++))) << 6 | (r2 = b64.indexOf(value.charAt(i++)));
 
 				result += r1 === 64 ? String.fromCharCode(bitmap >> 16 & 255)
 								: r2 === 64 ? String.fromCharCode(bitmap >> 16 & 255, bitmap >> 8 & 255)
@@ -234,7 +35,7 @@ if (typeof atob === "undefined" || atob === null) {
 	globalThis.atob = atob;
 }
 
-/** @type {Applications} */
+/** @type {Record<string,string>} */
 const apps = {
 	Chrome: "com.google.Chrome",
 	Edge: "com.microsoft.edgemac",
@@ -242,7 +43,7 @@ const apps = {
 	Safari: "com.apple.Safari",
 };
 
-/** @type {Browsers} */
+/** @type {Record<string,Contexts>} */
 const browsers = {
 	"nllm4000559023": {
 		main: apps.Edge,
@@ -251,6 +52,8 @@ const browsers = {
 	},
 };
 
+/** @typedef {"main"|"work"|"home"} Context */
+/** @typedef {Record<Context,string>} Contexts */
 /** @type {Contexts} */
 const defaultBrowsers = {
 	main: apps.Safari,
@@ -260,7 +63,7 @@ const defaultBrowsers = {
 
 /**
  * @param {Context} contextName
- * @returns {import("./finicky.d").Finicky.BrowserFn}
+ * @returns {BrowserResolver}
  * */
 const getBrowser = (contextName) => (_) => {
 	console.log(finicky.getSystemInfo().localizedName.split(".")[0]);
@@ -271,72 +74,61 @@ const getBrowser = (contextName) => (_) => {
 };
 
 /**
-	* @param {string} prefix
-	* @returns {import("./finicky.d").Finicky.Rewrite}
-*/
+ * @param {string} prefix
+ * @returns {UrlRewriteRule}
+ */
 const prefixBGone = (prefix) => ({
-	match: ({ urlString }) => urlString.startsWith(prefix),
-	url: ({ urlString }) => decodeURIComponent(urlString.substring(prefix.length).replace(/%25/g, "%")).replaceAll(" ", "%20"),
+	match: ({ href }) => href.startsWith(prefix),
+	url: ({ href }) => decodeURIComponent(href.substring(prefix.length).replace(/%25/g, "%")).replaceAll(" ", "%20"),
 });
 
 /**
  * @param {string} host
  * @param {string} queryParam
- * @returns {import("./finicky.d").Finicky.Rewrite}
+ * @returns {UrlRewriteRule}
  */
 const redirectBGone = (host, queryParam) => ({
-	match: ({ url }) => url.host.endsWith(host),
-	url: ({ url }) => decodeURIComponent((new URLSearchParams(url.search)).get(queryParam).replace(/%25/g, "%")),
+	match: (url) => url.host.endsWith(host),
+	url: (url) => url.searchParams.get(queryParam).replace(/%25/g, "%"),
 });
 
 /**
  * @param {string} suffix
- * @returns {import("./finicky.d").Finicky.Rewrite}
+ * @returns {UrlRewriteRule}
  */
 const suffixBGone = (suffix) => ({
-	match: ({ urlString }) => urlString.endsWith(suffix),
-	url: ({ urlString }) => decodeURIComponent(urlString.substring(0, urlString.length - suffix.length).replace(/%25/g, "%")),
+	match: (url) => url.href.endsWith(suffix),
+	url: (url) => url.href.
+		substring(0, (url.href.length - suffix.length)).
+		replace(/%25/g, "%"),
 });
 
 /**
  * @param {string|RegExp} re
- * @returns {import("./finicky.d").Finicky.Rewrite}
+ * @returns {UrlRewriteRule}
  */
 const matchBGone = (re) => ({
-	match: ({ urlString }) => urlString.match(re),
-	url: ({ urlString }) => decodeURIComponent(urlString.replace(re, "").replace(/%25/g, "%")).replaceAll(" ", "%20"),
-});
-
-/**
- * @param {string} prefix
- * @returns {import("./finicky.d").Finicky.Rewrite}
- */
-const log = (prefix) => ({
-	match: () => true,
-	url: (params) => {
-		console.log(prefix + ": " + params.urlString);
-
-		return params.url;
-	},
+	match: ({ href }) => href.match(re),
+	url: (url) => url.href.
+		replace(re, "").
+		replace(/%25/g, "%").
+		replace( / /g, "%20"),
 });
 
 /**
  * @param {string} protocol
- * @returns {import("./finicky.d").Finicky.Rewrite}
+ * @returns {UrlRewriteRule}
  */
 const defaultProtocol = (protocol) => ({
-	match: ({ urlString }) => !urlString.match(/^[a-z]+:\/\//),
-	url: ({ urlString }) => protocol + "://" + urlString,
+	match: (url) => !url.href.match(/^[a-z]+:\/\//),
+	url: (url) => protocol + "://" + url.href,
 });
 
-/**
- * @type {import("./finicky.d").Finicky.URLFn}
- */
-const fuckOffMandrill = ({ url }) =>
-	JSON.parse(JSON.parse(atob((new URLSearchParams(url.search)).get("p"))).p).url;
+/** @type {UrlTransformer} */
+const fuckOffMandrill = (url) =>
+	JSON.parse(JSON.parse(atob(url.searchParams.get("p"))).p).url;
 
-/// <reference path="./finicky.d.ts" />
-/** @type {import("./finicky.d").Finicky.Config} */
+/** @type {FinickyConfig} */
 export default {
 	defaultBrowser: getBrowser("main"),
 	rewrite: [
@@ -427,28 +219,28 @@ export default {
 				};
 			},
 		},
-		// Youtu.be => Yattee
-		{
-			// https://youtu.be/T_O-NeTvUzs?feature=shared
-			match: ({ urlString }) => urlString.startsWith("https://youtu.be"),
-			// https://r.yattee.stream/watch?feature=shared&v=T_O-NeTvUzs
-			url: ({ urlString }) => "https://r.yattee.stream/watch?v=" + urlString.replace(/https:\/\/youtu\.be\/([^\?]+)(\?.*)?/, "$1"),
-		},
-		// Youtube => Yattee
-		{
-			match: ({ url }) => url.host == "youtube.com",
-			url: ({ url }) => "https://r.yattee.stream/watch?v=" + url.search.replace(/.*(v=[^&]+).*/, "$1"),
-		},
+		// // Youtu.be => Yattee
+		// {
+		// 	// https://youtu.be/T_O-NeTvUzs?feature=shared
+		// 	match: ({ href }) => href.startsWith("https://youtu.be"),
+		// 	// https://r.yattee.stream/watch?feature=shared&v=T_O-NeTvUzs
+		// 	url: ({ href }) => "https://r.yattee.stream/watch?v=" + href.replace(/https:\/\/youtu\.be\/([^\?]+)(\?.*)?/, "$1"),
+		// },
+		// // Youtube => Yattee
+		// {
+		// 	match: ({ url }) => url.host == "youtube.com",
+		// 	url: ({ url }) => "https://r.yattee.stream/watch?v=" + url.search.replace(/.*(v=[^&]+).*/, "$1"),
+		// },
 		// Mandrill => wrapped URL
 		{
-			match: ({ urlString }) => urlString.startsWith("https://mandrillapp.com/track/click/"),
+			match: ({ href }) => href.startsWith("https://mandrillapp.com/track/click/"),
 			url: fuckOffMandrill,
 		},
 		{
 			// https://realm-group-holdings-limited.app.loxo.co/agencies/11114/email_tracking/click?id=197135213&url=https%3A%2F%2Fdocs.google.com%2Fdocument%2Fd%2F1JLYlq2f4pwksRTO61r4-Dlnu9LIjn3ToV66pS2qvabA%2Fedit
 			match: ({ url }) => url.host == "realm-group-holdings-limited.app.loxo.co"
 				&& url.pathname.includes("/email_tracking/"),
-			url: ({ url }) => decodeURIComponent((new URLSearchParams(url.search)).get("url")),
+			url: (url) => decodeURIComponent(url.searchParams.get("url")),
 		},
 		// log("post-rewrite"),
 	],
@@ -461,17 +253,11 @@ export default {
 		// Work: Microsoft Teams handler
 		{
 			match: finicky.matchHostnames("teams.microsoft.com"),
-			browser: "com.microsoft.teams2",
-			url({ url }) {
-				return {
-					...url,
-					protocol: "msteams",
-				};
-			},
+			browser: "com.microsoft.teams2"
 		},
 		// Work: source apps
 		{
-			match: ({ opener }) =>
+			match: (_, { opener }) =>
 				[
 					"com.tinyspeck.slackmacgap",
 					"com.microsoft.teams2",
@@ -491,7 +277,7 @@ export default {
 		},
 		// Personal: social and IM apps
 		{
-			match: ({ opener }) =>
+			match: (_, { opener }) =>
 				[
 					"com.facebook.archon",
 					"ru.keepcoder.Telegram",
@@ -513,7 +299,7 @@ export default {
 		},
 		// General: apps that should open on the main browser directly
 		{
-			match: ({ opener }) =>
+			match: (_, { opener }) =>
 				[
 					"com.1password.1password"
 				].includes(opener.bundleId),
