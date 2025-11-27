1c22973a111090357fb2497c34287cb242496bf0
git@artero.dev <git@artero.dev>
2025-11-27 18:47:55 +0100

    chore: remove unused casks/apps
    Signed-off-by: William Artero <git@artero.dev>
    ps-id: c28179f7-650a-4d9f-b8bf-37792877ee42

--- settings/personal/homebrew.nix
+++ settings/personal/homebrew.nix
@@ -9,25 +9,21 @@
     "app-cleaner" # # Nektony App Cleaner & Uninstaller
     "appcleaner"
     "calibre"
-    "duckduckgo"
     "hakuneko"
     "image2icon"
     "keybase"
     "launchcontrol"
-    "launchpad-manager"
     "macfuse" # # needed by resilio-sync
     "macpass"
     "mate-translate"
     "msty" # # AI/LLM
     "netnewswire"
-    "onyx"
     "plex-htpc"
     "plexamp"
     "racket"
     "raindropio"
     "resilio-sync"
     "soundsource"
-    "spotify"
     "suspicious-package"
     "thingsmacsandboxhelper"
     "tiddly"
@@ -39,29 +35,19 @@
   homebrew.masApps = {
     # keep-sorted start
     "Apple Configurator" = 1037126344;
-    # "Be Focused Pro" = 961632517;
     "CCMenu" = 603117688;
     "DoMarks" = 1518886084;
     "GarageBand" = 682658836;
-    # "Just Press Record" = 1033342465;
     "Keynote" = 409183694;
     "Numbers" = 409203825;
     "OmniOutliner" = 1142578772;
-    # "OX Drive" = 818195014;
-    "Parcel" = 639968404;
-    # "Privacy Redirect" = 1578144015;
     "Save to Raindrop.io" = 1549370672;
     "StopTheMadness" = 1376402589;
-    "Supernote Partner" = 1494992020;
-    # "SwiftoDo Desktop" = 1143641091;
-    # "Tampermonkey Classic" = 1482490089;
     "TestFlight" = 899247664;
     "Things" = 904280696;
-    # "Userscripts-Mac-App" = 1463298887;
     "WireGuard" = 1451685025;
     "Xcode" = 497799835;
     "iMovie" = 408981434;
-    # "uBlacklist for Safari" = 1547912640;
     # keep-sorted end
   };
 }
