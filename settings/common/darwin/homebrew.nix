ffe263071fbdb78af1b4567ec7fee6ce0f6e0a5f
git@artero.dev <git@artero.dev>
2025-11-27 18:46:25 +0100

    feat: add qcachegrind
    This provides a callgrind viewer to work with e.g. remake or golang
    traces.
    
    Signed-off-by: William Artero <git@artero.dev>
    ps-id: d45cd33d-0f95-4555-a96d-5d71f34c3456

--- settings/common/darwin/homebrew.nix
+++ settings/common/darwin/homebrew.nix
@@ -13,19 +13,26 @@
     lib.mkMerge [
       (lib.optional hasApps "mas") # # used internally by brew masApps
       (lib.optional hasTaps "gh") # # used internally by brew taps
+      [
+        # keep-sorted start
+        "qcachegrind"
+        # keep-sorted end
+      ]
     ];
 
   homebrew.casks = [
+    # keep-sorted start
     "anytype"
     "automatic-mouse-mover"
     "bartender"
     "bruno"
     "das-keyboard-q"
     "displaylink-login-screen-ext"
-    (pkgs.lib.local.globalCask "displaylink-manager")
     "elgato-stream-deck"
     "flux-app"
     "sqlitestudio"
+    # keep-sorted end
+    (pkgs.lib.local.globalCask "displaylink-manager")
   ];
 
   homebrew.taps = [
