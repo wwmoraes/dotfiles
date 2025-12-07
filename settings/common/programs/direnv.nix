4da7e86efc5b505215574f18bf55f5f667cba936
git@artero.dev <git@artero.dev>
2025-12-07 13:33:57 +0100

    chore(direnv): dedupe reload
    ps-id: 7f465bad-458d-4ba6-87dd-850e2ac219c5
    Signed-off-by: William Artero <git@artero.dev>

--- settings/common/programs/direnv.nix
+++ settings/common/programs/direnv.nix
@@ -1,5 +1,4 @@
 {
-  lib,
   pkgs,
   ...
 }:
@@ -14,22 +13,4 @@
       };
     };
   };
-
-  home-manager.sharedModules = [
-    (
-      { config, ... }:
-      {
-        programs.fish.interactiveShellInit = lib.mkIf config.programs.direnv.enable ''
-          ## reload direnv now, otherwise it only triggers on dir change
-          ${lib.getExe config.programs.direnv.package} reload 2> /dev/null; or true
-        '';
-
-        programs.direnv = {
-          enable = true;
-          nix-direnv.enable = true;
-          silent = true;
-        };
-      }
-    )
-  ];
 }
