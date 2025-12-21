cb945555224c6de1b30db310597e342c98fa9b8d
git@artero.dev <git@artero.dev>
2025-12-21 16:14:33 +0100

    chore: cleanup vidar host configuration
    Signed-off-by: William Artero <git@artero.dev>
    ps-id: 6f30afb6-d5ba-470a-bb5c-99b03f77a242

--- hosts/vidar/default.nix
+++ hosts/vidar/default.nix
@@ -47,23 +47,15 @@
       # imports = [
       #   ../../users/william/share/home/programs/gpg
       # ];
-      programs.bash.enable = true;
-      services.gpg-agent = {
-        enable = lib.mkForce false;
-      };
+      programs.fish.enable = true;
+      services.gpg-agent.enable = lib.mkForce false;
       systemd.user.sessionVariables = {
         inherit (config.home-manager.users.root.home.sessionVariables) GNUPGHOME;
       };
     };
     william = {
-      programs = {
-        bash.enable = true;
-        fish.enable = true;
-      };
-
-      services.gpg-agent = {
-        enable = lib.mkForce false;
-      };
+      programs.fish.enable = true;
+      services.gpg-agent.enable = lib.mkForce false;
     };
   };
 
@@ -73,14 +65,10 @@
   networking.hostName = "vidar";
   networking.networkmanager.enable = true;
 
-  nix = {
-    settings = {
-      extra-experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-    };
-  };
+  nix.settings.extra-experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   programs = {
     dconf.enable = true; # needed by home-manager somehow
@@ -101,30 +89,6 @@
     #   MaxStartups 10:20:50
     # '';
   };
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.05"; # Did you read the comment?
 
   time.timeZone = "Europe/Amsterdam";
 
