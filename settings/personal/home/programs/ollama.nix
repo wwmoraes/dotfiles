36b589461eb224d7249e59cc2940dc25201b9d71
William Artero <git@artero.dev>
2025-12-30 18:43:10 +0100

    chore: configure ollama flash atention and KV cache

--- settings/personal/home/programs/ollama.nix
+++ settings/personal/home/programs/ollama.nix
@@ -14,8 +14,10 @@
       enable = true;
       config = {
         EnvironmentVariables = {
+          OLLAMA_FLASH_ATTENTION = "1";
           OLLAMA_HOST = "127.0.0.1:11434";
           OLLAMA_KEEP_ALIVE = "5m";
+          OLLAMA_KV_CACHE_TYPE = "q8_0";
         };
         KeepAlive = false;
         Label = "dev.artero.ollama";
