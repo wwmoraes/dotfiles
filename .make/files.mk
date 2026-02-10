6060d3b060ac111d57ee147146ccd8fb8f335cd4
William Artero <git@artero.dev>
2026-02-10 17:14:03 +0100

    build: fix flake generation
    Split between the files parts and the flake itself, as they have
    distinct dependencies and build commands.
    
    Signed-off-by: William Artero <git@artero.dev>
    ps-id: 4bbdf31f-5617-4be3-b4f4-10179a43ef9c

--- .make/files.mk
+++ .make/files.mk
@@ -1,21 +1,24 @@
 define FILES_SOURCES
 $(sort $(strip
-$(shell git grep --name-only --untracked 'flake-file' -- '*.nix')
 $(shell git grep --name-only --untracked 'files.files' -- '*.nix')
 ))
 endef
 define FILES_OUTPUTS
 $(sort $(strip
 .github/workflows/integration.yml
-flake.nix
 ))
 endef
 
 .PHONY: files
 #: Re-generates files managed by nix.
-files: ${FILES_OUTPUTS}
+files: flake.nix ${FILES_OUTPUTS}
 
 ${FILES_OUTPUTS} &: $(filter-out ${FILES_OUTPUTS},${FILES_SOURCES})
-	@$(if $?,git add -N $(sort $?),:)
+	@git add -N $^
 	nix run .#write-files
 	@touch ${FILES_OUTPUTS}
+
+flake.nix: $(filter-out flake.nix,$(shell git grep --name-only --untracked 'flake-file' -- '*.nix'))
+	@git add -N $^
+	nix run .#write-flake
+	@touch $@
