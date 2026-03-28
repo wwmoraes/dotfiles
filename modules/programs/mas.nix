6e89b3303dd3263efbc350173a5d87b72093cbc5
William Artero <git@artero.dev>
2026-03-28 15:20:15 +0100

    chore: add tampermonkey
    Signed-off-by: William Artero <git@artero.dev>
    ps-id: c2e6159c-f6cd-4684-8fee-239d502529b9

--- modules/programs/mas.nix
+++ modules/programs/mas.nix
@@ -7,6 +7,7 @@
       "Image2Icon" = 992115977;
       "OmniOutliner" = 1142578772;
       "StopTheMadness" = 1376402589;
+      "Tampermonkey" = 6738342400;
       "WireGuard" = 1451685025;
       # keep-sorted end
     };
