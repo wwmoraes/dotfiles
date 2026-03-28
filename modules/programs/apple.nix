77ac6a713a3db38b8756c06d793ae4275c37072e
William Artero <git@artero.dev>
2026-03-28 15:17:47 +0100

    refactor: move Apple apps to its own module
    Signed-off-by: William Artero <git@artero.dev>
    ps-id: 62df789b-a6a1-4755-a8b4-7f4687126c8a

---
+++ modules/programs/apple.nix
@@ -0,0 +1,15 @@
+{
+  flake.modules.darwin.personal = {
+    homebrew.masApps = {
+      # keep-sorted start
+      "Apple Configurator" = 1037126344;
+      "GarageBand" = 682658836;
+      "Keynote" = 409183694;
+      "Numbers" = 409203825;
+      "TestFlight" = 899247664;
+      "Xcode" = 497799835;
+      "iMovie" = 408981434;
+      # keep-sorted end
+    };
+  };
+}
--- modules/programs/mas.nix
+++ modules/programs/mas.nix
@@ -2,19 +2,12 @@
   flake.modules.darwin.personal = {
     homebrew.masApps = {
       # keep-sorted start
-      "Apple Configurator" = 1037126344;
       "CCMenu" = 603117688;
       "DoMarks" = 1518886084;
-      "GarageBand" = 682658836;
       "Image2Icon" = 992115977;
-      "Keynote" = 409183694;
-      "Numbers" = 409203825;
       "OmniOutliner" = 1142578772;
       "StopTheMadness" = 1376402589;
-      "TestFlight" = 899247664;
       "WireGuard" = 1451685025;
-      "Xcode" = 497799835;
-      "iMovie" = 408981434;
       # keep-sorted end
     };
   };
