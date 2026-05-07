39ba0e43e817e2215fa49acaa35e0c1199ba7585
William Artero <git@artero.dev>
2026-05-07 13:38:22 +0200

    chore: update Apple MAS product IDs
    Apple decided to change the ID instead of updating the product
    in-place...
    
    Signed-off-by: William Artero <git@artero.dev>
    ps-id: dc0c6a3f-fc1d-44c9-bff2-22c0c933de8a

--- modules/programs/apple.nix
+++ modules/programs/apple.nix
@@ -4,8 +4,8 @@
       # keep-sorted start
       "Apple Configurator" = 1037126344;
       "GarageBand" = 682658836;
-      "Keynote" = 409183694;
-      "Numbers" = 409203825;
+      "Keynote" = 361285480;
+      "Numbers" = 361304891;
       "TestFlight" = 899247664;
       "Xcode" = 497799835;
       "iMovie" = 408981434;
