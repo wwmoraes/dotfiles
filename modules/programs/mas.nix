4b70110d9ea15d0903b87024f1e19e8810e5df49
William Artero <git@artero.dev>
2026-08-13 23:58:47 +0200

    chore: update OmniOutliner

--- modules/programs/mas.nix
+++ modules/programs/mas.nix
@@ -5,7 +5,7 @@
       "CCMenu" = 603117688;
       "DoMarks" = 1518886084;
       "Image2Icon" = 992115977;
-      "OmniOutliner" = 1142578772;
+      "OmniOutliner" = 6474965689;
       "StopTheMadness" = 1376402589;
       "Tampermonkey" = 6738342400;
       "WireGuard" = 1451685025;
