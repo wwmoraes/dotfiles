f93598b25bfbc41eab1a5d22caaf3e6c7835d8a2
William Artero <git@artero.dev>
2026-08-13 15:52:40 +0200

    chore: disable qcachegrind

--- modules/programs/qcachegrind.nix
+++ modules/programs/qcachegrind.nix
@@ -1,5 +1,6 @@
 {
-  flake.modules.darwin.development'personal = {
+  # TODO use gprof2dot locally to avoid qtbase/dbus/dozens of other dependencies
+  flake.modules.darwin.development'personal'disabled = {
     homebrew.brews = [
       "qcachegrind"
     ];
