777606e588345b1040311836f5ec6a3b9688f58e
William Artero <git@artero.dev>
2026-08-17 17:11:44 +0200

    refactor: homebrew support in home-manager
    Adds forward support for brews, casks and MAS apps in home-manager. It
    mostly disrupts the abstraction layer of "home configurations" aside
    from casks, which brew can install in ~/Applications (and MacOS supports
    it natively).
    
    This is a transitive option while there's options unavailable in
    Nixpkgs/NUR, making programs' and services' configurations more
    straightforward on Darwin, keeping options definitions close to NixOS
    modules.
    
    There's also a placeholder for NixOS to avoid home-manager modules
    breaking there. In practice HM modules with Darwin-specific needs should
    be a shared module declared within a nix-darwin flake module. Or at
    least the specific part of it.
    
    Homebrew does support Linux however I've never used it nor will I bother
    as it is pointless to do so.

--- modules/inputs/nix-darwin.nix
+++ modules/inputs/nix-darwin.nix
@@ -1,6 +1,78 @@
+{ lib, ... }:
 {
   flake-file.inputs.nix-darwin = {
     inputs.nixpkgs.follows = "nixpkgs";
     url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
   };
+
+  # add support for user-level defined brew components
+  flake.modules.darwin.default = { config, options, ... }: {
+    home-manager.sharedModules = [
+      {
+        options.homebrew = {
+          inherit (options.homebrew)
+            brews
+            casks
+            masApps
+            prefix
+            ;
+        };
+      }
+    ];
+
+    homebrew.brews =
+      config.home-manager.users
+      |> builtins.attrValues
+      |> map (attrs: attrs.homebrew.brews)
+      |> builtins.concatLists;
+
+    homebrew.casks =
+      config.home-manager.users
+      |> builtins.attrValues
+      |> map (attrs: attrs.homebrew.casks)
+      |> builtins.concatLists;
+
+    homebrew.masApps =
+      config.home-manager.users
+      |> builtins.attrValues
+      |> map (attrs: attrs.homebrew.masApps)
+      |> lib.mergeAttrsList;
+  };
+
+  flake.modules.nixos.default =
+    { config, ... }:
+    let
+      cfg = config.homebrew;
+    in
+    {
+      options.homebrew = with lib.types; {
+        brews = lib.mkOption {
+          type = listOf anything;
+        };
+        casks = lib.mkOption {
+          type = listOf anything;
+        };
+        masApps = lib.mkOption {
+          type = attrsOf ints.positive;
+        };
+        prefix = lib.mkOption {
+          type = str;
+          default = "/usr/local";
+        };
+      };
+
+      config = {
+        warnings = builtins.concatLists [
+          (lib.optional (
+            builtins.length cfg.brews > 0
+          ) "Homebrew brews aren't supported on NixOS yet. Install those packages using other means.")
+          (lib.optional (
+            builtins.length cfg.casks > 0
+          ) "Homebrew casks aren't supported on NixOS yet. Install those packages using other means.")
+          (lib.optional (
+            builtins.length (builtins.attrNames cfg.masApps) > 0
+          ) "Homebrew MAS apps are exclusive to Darwin. Install applications using other means on NixOS.")
+        ];
+      };
+    };
 }
